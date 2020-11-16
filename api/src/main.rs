2889f7f606b56e9554f44a01a1b2a35f5b72d578
David Sferruzza <david.sferruzza@gmail.com>
2020-11-16 16:41:45 +0100

    feat: add DB connection

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,33 +1,52 @@
 use actix_web::{middleware::Logger, web, App, HttpResponse, HttpServer, Responder};
 use clap::{ArgSettings::HideEnvValues, Clap};
+use log::{info, trace};
+use sqlx::postgres::{PgPool, PgPoolOptions};
 
 #[derive(Debug, Clone, Clap)]
 #[clap(author, about, version)]
 struct Config {
     /// IP address on which to start the HTTP server
-    #[clap(long = "ip", env = "IP", default_value = "127.0.0.1")]
+    #[clap(long, env, default_value = "127.0.0.1")]
     ip: String,
 
     /// Port on which to start the HTTP server
-    #[clap(long = "port", env = "PORT", default_value = "8080")]
+    #[clap(long, env, default_value = "8080")]
     port: String,
 
     /// Database URL (with credentials)
     #[clap(long, env, setting = HideEnvValues)]
     database_url: String,
+
+    /// Maximum number of connection to database
+    #[clap(long, env, default_value = "5")]
+    max_db_connections: u32,
 }
 
 /// The app state
-#[derive(Clone)]
-pub struct State {}
+#[derive(Debug, Clone)]
+pub struct State {
+    db: PgPool,
+}
 
 #[actix_web::main]
-async fn main() -> std::io::Result<()> {
+async fn main() -> anyhow::Result<()> {
     env_logger::init();
     let config = Config::parse();
+    trace!("Starting Hook0 API");
+
+    // Create a DB connection pool
+    let pool = PgPoolOptions::new()
+        .max_connections(config.max_db_connections)
+        .connect(&config.database_url)
+        .await?;
+    info!(
+        "Started a pool of maximum {} DB connections",
+        &config.max_db_connections
+    );
 
     // Initialize state
-    let initial_state = State {};
+    let initial_state = State { db: pool };
 
     // Run web server
     HttpServer::new(move || {
@@ -39,6 +58,7 @@ async fn main() -> std::io::Result<()> {
     .bind(&format!("{}:{}", config.ip, config.port))?
     .run()
     .await
+    .map_err(|e| e.into())
 }
 
 async fn index() -> impl Responder {
