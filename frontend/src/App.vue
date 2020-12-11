1b9fb0295f2f9a362790bd6d10df2339cd4819af
François-Guillaume Ribreau <github@fgribreau.com>
2020-12-11 17:13:45 +0100

    feat: frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import {components} from './components';
+
+export default {
+  name: "App",
+  components: components
+};
+</script>
+
+<style>
+</style>
