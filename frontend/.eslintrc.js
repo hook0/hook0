f5df1748db1c14742139be1567f04ad098a6e6c8
François-Guillaume Ribreau <git@fgribreau.com>
2021-04-16 10:08:40 +0200

    feat

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,16 +1,28 @@
 module.exports = {
   root: true,
+
   env: {
     node: true,
   },
+
   extends: ['plugin:vue/vue3-essential', 'plugin:vue/recommended', '@vue/prettier'],
+
   parserOptions: {
     ecmaVersion: 2017,
+    parser: '@typescript-eslint/parser',
   },
+
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'comma-style': ['error', 'last'],
     'vue/no-multiple-template-root': 'off',
   },
+
+  extends: [
+    'plugin:vue/vue3-essential',
+    'plugin:vue/recommended',
+    '@vue/prettier',
+    '@vue/typescript',
+  ],
 };
--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,179 +1,59 @@
 <template>
-  <div class="h-screen flex overflow-hidden bg-gray-100">
-    <!-- Static sidebar for desktop -->
-    <div class="hidden md:flex md:flex-shrink-0">
-      <div class="flex flex-col w-64">
-        <!-- Sidebar component, swap this element with another sidebar if you like -->
-        <div class="flex flex-col h-0 flex-1">
-          <div class="flex items-center h-16 flex-shrink-0 px-4 bg-gray-900">
-            <logo></logo>
-          </div>
-          <div class="flex-1 flex flex-col overflow-y-auto">
-            <nav class="flex-1 px-2 py-4 bg-gray-800 space-y-1">
-              <menu-item :active="true" name="Dashboard">
-                <icon kind="home"></icon>
-              </menu-item>
-
-              <menu-item :active="false" name="Organizations">
-                <icon kind="team"></icon>
-              </menu-item>
-
-              <menu-item :active="false" name="Applications">
-                <icon kind="document"></icon>
-              </menu-item>
-
-              <menu-item :active="false" name="Event Types">
-                <icon kind="document"></icon>
-              </menu-item>
-
-              <menu-item :active="false" name="Webhooks">
-                <icon kind="folder"></icon>
-              </menu-item>
-
-              <menu-item :active="false" name="Logs">
-                <icon kind="calendar"></icon>
-              </menu-item>
-
-              <menu-item :active="false" name="Settings">
-                <icon kind="document"></icon>
-              </menu-item>
-            </nav>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="flex flex-col w-0 flex-1 overflow-hidden">
-      <div class="relative z-10 flex-shrink-0 flex h-16 bg-white shadow">
-        <button
-          class="px-4 border-r border-gray-200 text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500 md:hidden">
-          <span class="sr-only">Open sidebar</span>
-          <!-- Heroicon name: menu-alt-2 -->
-          <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"
-               aria-hidden="true">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7"/>
-          </svg>
-        </button>
-        <div class="flex-1 px-4 flex justify-between">
-          <div class="flex-1 flex">
-            <form class="w-full flex md:ml-0" action="#" method="GET">
-              <label for="search_field" class="sr-only">Search</label>
-              <div class="relative w-full text-gray-400 focus-within:text-gray-600">
-                <div class="absolute inset-y-0 left-0 flex items-center pointer-events-none">
-                  <!-- Heroicon name: search -->
-                  <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
-                       aria-hidden="true">
-                    <path fill-rule="evenodd"
-                          d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                          clip-rule="evenodd"/>
-                  </svg>
-                </div>
-                <input id="search_field"
-                       class="block w-full h-full pl-8 pr-3 py-2 border-transparent text-gray-900 placeholder-gray-500 focus:outline-none focus:placeholder-gray-400 focus:ring-0 focus:border-transparent sm:text-sm"
-                       placeholder="Search" type="search" name="search">
-              </div>
-            </form>
-          </div>
-          <div class="ml-4 flex items-center md:ml-6">
-            <button
-              class="bg-white p-1 rounded-full text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-              <span class="sr-only">View notifications</span>
-              <!-- Heroicon name: bell -->
-              <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                   stroke="currentColor" aria-hidden="true">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                      d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"/>
-              </svg>
-            </button>
-
-            <!-- Profile dropdown -->
-            <div class="ml-3 relative">
-              <div>
-                <button
-                  class="max-w-xs bg-white flex items-center text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                  id="user-menu" aria-haspopup="true">
-                  <span class="sr-only">Open user menu</span>
-                  <img class="h-8 w-8 rounded-full"
-                       src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                       alt="">
-                </button>
-              </div>
-              <!--
-                Profile dropdown panel, show/hide based on dropdown state.
-
-                Entering: "transition ease-out duration-100"
-                  From: "transform opacity-0 scale-95"
-                  To: "transform opacity-100 scale-100"
-                Leaving: "transition ease-in duration-75"
-                  From: "transform opacity-100 scale-100"
-                  To: "transform opacity-0 scale-95"
-              -->
-              <div
-                class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5"
-                role="menu" aria-orientation="vertical" aria-labelledby="user-menu">
-                <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Your
-                  Profile</a>
-
-                <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Settings</a>
-
-                <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Sign out</a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <main class="flex-1 relative overflow-y-auto focus:outline-none" tabindex="0">
-        <div class="py-6">
-          <div class="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
-            <h1 class="text-2xl font-semibold text-gray-900">Dashboard</h1>
-          </div>
-          <div class="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
-            <!-- Replace with your content -->
-            <div class="py-4">
-              <div class="border-4 border-dashed border-gray-200 rounded-lg h-96">
-                <Promised :promise="applications$">
-                  <!-- Use the "pending" slot to display a loading message -->
-                  <template #pending>
-                    <p>Loading...</p>
-                  </template>
-                  <!-- The default scoped slot will be used as the result -->
-                  <template v-slot="applications">
-                    <ul>
-                      <li v-for="application in applications" :key="application.application__id">{{ application.application__id }} - {{ application.name }}</li>
-                    </ul>
-                  </template>
-                  <!-- The "rejected" scoped slot will be used if there is an error -->
-                  <template #rejected="error">
-                    <p>Error: {{ error.message }}</p>
-                  </template>
-                </Promised>
-              </div>
-            </div>
-            <!-- /End replace -->
-          </div>
-        </div>
-      </main>
-    </div>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>
+      For a guide and recipes on how to configure / customize this project,<br>
+      check out the
+      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+    </p>
+    <h3>Installed CLI Plugins</h3>
+    <ul>
+      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript" target="_blank" rel="noopener">typescript</a></li>
+    </ul>
+    <h3>Essential Links</h3>
+    <ul>
+      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
+      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
+      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
+      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
+      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
+    </ul>
+    <h3>Ecosystem</h3>
+    <ul>
+      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
+      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
+      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
+      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
+      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+    </ul>
   </div>
 </template>
 
-<script>
-import Icon from "@/components/Icon";
-import APIClient from '../APIClient';
+<script lang="ts">
+import { Vue, Options } from 'vue-class-component'
 
-export default {
-  name: "HelloWorld",
-  components: {Icon},
-  data: () => ({ applications$: new Promise(() => {}) }),
+@Options
+export default class HelloWorld extends Vue {
   props: {
-    msg: String
-  },
-  created() {
-    this.applications$ = APIClient.applications.list();
-  },
-};
+    msg: String,
+  }
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
 </style>
