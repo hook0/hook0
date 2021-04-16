195913649019a1a9ab94a7978311be24576cb68c
David Sferruzza <david.sferruzza@gmail.com>
2021-04-16 11:27:31 +0200

    feat: add endpoint to list organizations

---
+++ api/src/handlers/organizations.rs
@@ -0,0 +1,33 @@
+use actix_web_middleware_keycloak_auth::UnstructuredClaims;
+use paperclip::actix::{
+    api_v2_operation,
+    web::{Json, ReqData},
+    Apiv2Schema,
+};
+use serde::Serialize;
+use uuid::Uuid;
+
+use crate::errors::*;
+use crate::iam::extract_organizations;
+
+#[derive(Debug, Serialize, Apiv2Schema)]
+#[allow(non_snake_case)]
+pub struct Organization {
+    pub organization__id: Uuid,
+    pub role: String,
+}
+
+/// List organizations
+#[api_v2_operation]
+pub async fn list(
+    unstructured_claims: ReqData<UnstructuredClaims>,
+) -> Result<Json<Vec<Organization>>, UnexpectedError> {
+    let organizations = extract_organizations(&unstructured_claims)
+        .iter()
+        .map(|(id, role)| Organization {
+            organization__id: *id,
+            role: role.to_string(),
+        })
+        .collect();
+    Ok(Json(organizations))
+}
