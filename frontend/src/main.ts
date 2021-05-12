2cde0e764beb11e157fedaae57b9a0c38c8d84ef
François-Guillaume Ribreau <git@fgribreau.com>
2021-05-12 23:18:16 +0200

    feat: components

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -8,12 +8,26 @@ import { Promised } from 'vue-promised';
 import components from './components';
 
 import Root from './Root.vue';
+import { RouteLocation } from 'vue-router';
 
 // Create and mount the root instance.
 const app = createApp(Root);
 
 // Vue - UIRouter
 app.use(router);
+
+// wrap router in order to keep our organization_id when present
+app.config.globalProperties.$router.push = (function(push) {
+  return function(to: RouteLocation) {
+    return push({
+      ...to,
+      params: {
+        ...to.params,
+        organization_id: app.config.globalProperties.$route.params.organization_id || null,
+      },
+    });
+  };
+})(app.config.globalProperties.$router.push);
 
 // Vue - promised
 app.component('Promised', Promised);
