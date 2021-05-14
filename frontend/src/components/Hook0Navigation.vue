3c1141fab6b06ac9ce69e9b6a5b6e314aa6adeac
François-Guillaume Ribreau <git@fgribreau.com>
2021-05-14 10:20:55 +0200

    feat: application listing and application create

---
+++ frontend/src/components/Hook0Navigation.vue
@@ -0,0 +1,55 @@
+<template>
+  <nav class="bg-white shadow">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div class="flex justify-between h-16">
+        <div class="flex">
+          <div class="flex-shrink-0 flex items-center">
+            <router-link class="navbar-item" :to="{ path: '/' }">
+              <img src="../assets/logo.png" width="28" height="28" />
+            </router-link>
+          </div>
+          <div class="navigation-items">
+            <a href="/" class="navigation-item navigation-item--active">
+              {{ $t('menu.deploymentList') }}
+            </a>
+            <a class="navigation-item">
+              {{ $t('menu.teamList') }}
+            </a>
+          </div>
+        </div>
+        <div class="hidden sm:ml-6 sm:flex sm:items-center">
+          <slot name="right"></slot>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'hook0-navigation',
+  components: {},
+  data() {
+    return {};
+  },
+  methods: {},
+  mounted() {},
+  computed: {},
+};
+</script>
+
+<style lang="scss" scoped>
+.footer-container {
+  @apply max-w-7xl mx-auto py-12 px-4 overflow-hidden sm:px-6 lg:px-8;
+}
+.navigation-items {
+  @apply hidden sm:ml-6 sm:flex sm:space-x-8;
+}
+.navigation-item {
+  @apply border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium;
+}
+
+.navigation-item--active {
+  @apply border-indigo-500 text-gray-900 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium;
+}
+</style>
--- frontend/src/pages/applications/ApplicationsList.vue
+++ frontend/src/pages/applications/ApplicationsList.vue
@@ -6,25 +6,31 @@
     </template>
     <!-- The default scoped slot will be used as the result -->
     <template #default="applications">
-      <div>
-        <div class="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
-          <h1 class="text-2xl font-semibold text-gray-900">Applications</h1>
-        </div>
-        <div class="max-w-7xl mx-auto px-4 sm:px-6 md:px-8 h-96">
-          <div class="py-4">
-            <template v-if="applications.length > 0">
-              <ul>
-                <li v-for="application in applications" :key="application.application_id">
-                  {{ application.application_id }} - {{ application.name }}
-                </li>
-              </ul>
+      <hook0-card>
+        <hook0-card-header>
+          <template v-slot:header>
+            Applications
+          </template>
+          <template v-slot:subtitle>
+            Each application send events to Hook0 API and Hook0 dispatch these extends to customers through webhooks
+          </template>
+        </hook0-card-header>
+
+        <hook0-card-content v-if="applications.length > 0">
+          <hook0-card-content-line v-for="application in applications" :key="application.application_id">
+            <template v-slot:label>
+              {{ application.name }}
             </template>
-            <template v-if="applications.length === 0">
-              <hook0-button class="primary" @click="$router.push({name:routes.ApplicationsNew})">PLop</hook0-button>
+            <template v-slot:content>
+              id: {{ application.application_id }}
             </template>
-          </div>
-        </div>
-      </div>
+          </hook0-card-content-line>
+        </hook0-card-content>
+
+        <hook0-card-footer>
+          <hook0-button class="primary" type="button" @click="$router.push({name:routes.ApplicationsNew})">Create new application</hook0-button>
+        </hook0-card-footer>
+      </hook0-card>
     </template>
     <!-- The "rejected" scoped slot will be used if there is an error -->
     <template #rejected="error">
@@ -38,9 +44,15 @@ import {Application, list} from './ApplicationService';
 import {Options, Vue} from 'vue-class-component';
 import Hook0Button from "@/components/Hook0Button.vue";
 import {routes} from "@/routes";
+import Hook0CardContentLine from "@/components/Hook0CardContentLine.vue";
+import Hook0CardContent from "@/components/Hook0CardContent.vue";
+import Hook0CardFooter from "@/components/Hook0CardFooter.vue";
+import Hook0CardHeader from "@/components/Hook0CardHeader.vue";
+import Hook0Card from "@/components/Hook0Card.vue";
+import Hook0Input from "@/components/Hook0Input.vue";
 
 @Options({
-  components: {Hook0Button},
+  components: {Hook0Button, Hook0CardContentLine, Hook0CardContent, Hook0CardFooter, Hook0CardHeader, Hook0Card, Hook0Input, Hook0Button},
   props: {
     msg: String
   },
