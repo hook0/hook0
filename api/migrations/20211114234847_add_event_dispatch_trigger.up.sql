e1e9a0b8b3b3553474504d6fea4490d7b39b49c6
David Sferruzza <david.sferruzza@gmail.com>
2021-11-15 01:15:00 +0100

    fix: event dispatch

---
+++ api/migrations/20211114234847_add_event_dispatch_trigger.up.sql
@@ -0,0 +1,34 @@
+create or replace function event.dispatch()
+    returns trigger
+    language plpgsql
+as
+$$
+declare
+    key text;
+    value text;
+    subscription_id uuid;
+begin
+    for key, value in select * from jsonb_each_text(new.labels) limit 50
+        loop
+            for subscription_id in
+                select subscription__id
+                from webhook.subscription
+                where is_enabled
+                  and label_key = key
+                  and label_value = value
+                loop
+                    raise notice '[event %] matching subscription: %', new.event__id, subscription_id;
+                    insert into webhook.request_attempt (event__id, subscription__id)
+                    values (new.event__id, subscription_id);
+                end loop;
+        end loop;
+    update event.event set dispatched_at = statement_timestamp() where event__id = new.event__id;
+    return new;
+end;
+$$;
+
+create trigger event_dispatch
+    after insert
+    on event.event
+    for each row
+    execute function event.dispatch();
