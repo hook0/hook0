4ecb33ce070f34a0e8ccc1e4eb556eb8fec05627
David Sferruzza <david.sferruzza@gmail.com>
2021-12-08 20:08:28 +0100

    chore: update front dependencies & improve linter support

--- frontend/src/components/Hook0CardContentLine.vue
+++ frontend/src/components/Hook0CardContentLine.vue
@@ -9,16 +9,12 @@
   </div>
 </template>
 
-<script>
+<script lang="ts">
 export default {
   name: 'hook0-card-content-line',
-  components: {},
   data() {
     return {};
   },
-  methods: {},
-  mounted() {},
-  computed: {},
 };
 </script>
 
