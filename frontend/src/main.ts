d5f1c2e6282ff2e24d6602e7ba1e5942da1b450c
David Sferruzza <david.sferruzza@gmail.com>
2021-12-09 16:36:35 +0100

    chore: enable more Typescript lints & fix corresponding violations

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,14 +2,14 @@
 import './tailwind.css';
 
 // Setup main app
-import { createApp } from 'vue';
+import { createApp, Plugin } from 'vue';
 import router from './router';
 import { Promised } from 'vue-promised';
 // import components from './components';
 import { KeycloakPlugin } from './iam';
 
 import Root from './Root.vue';
-import { RouteLocation } from 'vue-router';
+import { RouteLocation, Router } from 'vue-router';
 
 // Create and mount the root instance.
 const app = createApp(Root);
@@ -21,17 +21,24 @@ app.use(router);
 app.use(KeycloakPlugin);
 
 // wrap router in order to keep our organization_id when present
-app.config.globalProperties.$router.push = (function(push) {
-  return function(to: RouteLocation) {
-    return push({
-      ...to,
-      params: {
-        ...to.params,
-        organization_id: app.config.globalProperties.$route.params.organization_id || null,
-      },
-    });
-  };
-})(app.config.globalProperties.$router.push);
+const RouterOrgPlugin: Plugin = {
+  install: (app, _options) => {
+    const router = app.config.globalProperties.$router as Router;
+    router.push = ((push) => {
+      return (to: RouteLocation) => {
+        const route = app.config.globalProperties.$route as RouteLocation;
+        return push({
+          ...to,
+          params: {
+            ...to.params,
+            organization_id: route.params.organization_id || null,
+          },
+        });
+      };
+    })(router.push);// eslint-disable-line @typescript-eslint/unbound-method
+  }
+};
+app.use(RouterOrgPlugin);
 
 // Vue - promised
 app.component('Promised', Promised);
