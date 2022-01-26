715a8db3b3b9888dd0f03327da59bfcf0c2d344e
François-Guillaume Ribreau <git@fgribreau.com>
2022-01-26 15:00:11 +0100

    feat(api): add openapi validation

---
+++ frontend/src/components/Hook0Table.vue
@@ -0,0 +1,20 @@
+<template>
+  <ul class="list" v-bind="{ ...$props, ...$attrs }">
+    <slot></slot>
+  </ul>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'hook0-list',
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.list {
+  @apply border border-gray-200 rounded-md divide-y divide-gray-200;
+}
+</style>
