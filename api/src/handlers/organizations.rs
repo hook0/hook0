561d9393fb92dab550d3788e5185c242a30134cc
David Sferruzza <david.sferruzza@gmail.com>
2022-02-16 17:52:22 +0100

    feat: add endpoints to invite or remove users from organizations

--- api/src/handlers/organizations.rs
+++ api/src/handlers/organizations.rs
@@ -4,14 +4,15 @@ use paperclip::actix::{
     web::{Data, Json, Path},
     Apiv2Schema,
 };
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 use sqlx::query_as;
 use std::collections::HashMap;
 use std::str::FromStr;
 use uuid::Uuid;
+use validator::Validate;
 
 use crate::iam::{AuthProof, Role, GROUP_SEP, ORGA_GROUP_PREFIX};
-use crate::keycloak_api::KeycloakApi;
+use crate::keycloak_api::{Group, KeycloakApi};
 use crate::problems::Hook0Problem;
 
 #[derive(Debug, Serialize, Apiv2Schema)]
@@ -162,7 +163,7 @@ pub async fn get(
                 let role = g
                     .path
                     .rsplit_once(GROUP_SEP)
-                    .and_then(|(_, str)| Role::from_str(str).ok())
+                    .and_then(|(_, str)| Role::from_string_with_prefix(str))
                     .unwrap_or(Role::Viewer);
                 RoleGroup { id: &g.id, role }
             });
@@ -215,4 +216,147 @@ pub async fn get(
         }
         None => Err(Hook0Problem::NotFound),
     }
+}
+
+#[derive(Debug, Serialize, Deserialize, Apiv2Schema, Validate)]
+pub struct Grant {
+    #[validate(non_control_character, email, length(max = 100))]
+    email: String,
+    role: String,
+}
+
+#[api_v2_operation(
+    summary = "Invite a user to an organization",
+    description = "",
+    operation_id = "organizations.grant",
+    consumes = "application/json",
+    produces = "application/json",
+    tags("Organizations Management")
+)]
+pub async fn grant(
+    state: Data<crate::State>,
+    auth: AuthProof,
+    organization_id: Path<Uuid>,
+    body: Json<Grant>,
+) -> Result<Json<Grant>, Hook0Problem> {
+    if auth
+        .can_access_organization(&organization_id, &Role::Editor)
+        .await
+        .is_none()
+    {
+        return Err(Hook0Problem::Forbidden);
+    }
+
+    if let Err(e) = body.validate() {
+        return Err(Hook0Problem::Validation(e));
+    }
+
+    match Role::from_str(&body.role) {
+        Ok(role) => {
+            let role_group_name = role.string_with_prefix();
+
+            let keycloak_api = KeycloakApi::new(
+                &state.keycloak_url,
+                &state.keycloak_realm,
+                &state.keycloak_client_id,
+                &state.keycloak_client_secret,
+            )
+            .await?;
+
+            let u = keycloak_api.get_user_by_email(&body.email).await?;
+            let g = keycloak_api
+                .lookup_group_by_name(&format!("{ORGA_GROUP_PREFIX}{organization_id}"))
+                .await?;
+            match (u, g) {
+                (Some(user), Some(group)) => {
+                    let root_group = keycloak_api.get_group(&group.id).await?;
+                    let role_group = root_group
+                        .sub_groups
+                        .iter()
+                        .find(|g| g.name == role_group_name)
+                        .ok_or(Hook0Problem::NotFound)?;
+
+                    remove_user_from_all_sub_groups(&keycloak_api, &user.id, &root_group).await?;
+                    keycloak_api
+                        .add_user_to_group(&user.id, &role_group.id)
+                        .await?;
+                    Ok(body)
+                }
+                _ => Err(Hook0Problem::NotFound),
+            }
+        }
+        Err(_) => Err(Hook0Problem::InvalidRole),
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize, Apiv2Schema)]
+pub struct Revoke {
+    user_id: Uuid,
+}
+
+#[api_v2_operation(
+    summary = "Revoke a user's access to an organization",
+    description = "",
+    operation_id = "organizations.revoke",
+    consumes = "application/json",
+    produces = "application/json",
+    tags("Organizations Management")
+)]
+pub async fn revoke(
+    state: Data<crate::State>,
+    auth: AuthProof,
+    organization_id: Path<Uuid>,
+    body: Json<Revoke>,
+) -> Result<Json<Revoke>, Hook0Problem> {
+    if auth
+        .can_access_organization(&organization_id, &Role::Editor)
+        .await
+        .is_none()
+    {
+        return Err(Hook0Problem::Forbidden);
+    }
+
+    let keycloak_api = KeycloakApi::new(
+        &state.keycloak_url,
+        &state.keycloak_realm,
+        &state.keycloak_client_id,
+        &state.keycloak_client_secret,
+    )
+    .await?;
+
+    match keycloak_api
+        .lookup_group_by_name(&format!("{ORGA_GROUP_PREFIX}{organization_id}"))
+        .await?
+    {
+        Some(group) => {
+            let root_group = keycloak_api.get_group(&group.id).await?;
+            remove_user_from_all_sub_groups(&keycloak_api, &body.user_id, &root_group).await?;
+
+            Ok(body)
+        }
+        None => Err(Hook0Problem::NotFound),
+    }
+}
+
+async fn remove_user_from_all_sub_groups(
+    keycloak_api: &KeycloakApi,
+    user_id: &Uuid,
+    root_group: &Group,
+) -> Result<(), Hook0Problem> {
+    let root_group_id = [&root_group.id].into_iter();
+    let sub_groups_ids = root_group.sub_groups.iter().map(|g| &g.id);
+    let groups_ids = root_group_id.chain(sub_groups_ids);
+
+    for group_id in groups_ids {
+        let members = keycloak_api.get_group_members(group_id).await?;
+        for member in members.iter() {
+            if &member.id == user_id {
+                keycloak_api
+                    .remove_user_from_group(user_id, group_id)
+                    .await?;
+            }
+        }
+    }
+
+    Ok(())
 }
