39e13848f0b0bbec8bfd57fd00c514190955d476
François-Guillaume Ribreau <git@fgribreau.com>
2022-03-11 11:43:01 +0100

    feat: add footer + universal button

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -1,8 +1,15 @@
-import { App, Component } from 'vue';
+import {App, Component} from 'vue';
 
-export { default as Logo } from './Logo.vue';
-export { default as MenuItem } from './MenuItem.vue';
-export { default as Icon } from './Icon.vue';
+export {default as Logo} from './Logo.vue';
+export {default as MenuItem} from './MenuItem.vue';
+export {default as Icon} from './Icon.vue';
+export {default as Hook0Button} from "@/components/Hook0Button.vue";
+export {default as Hook0Input} from "@/components/Hook0Input.vue";
+export {default as Hook0Card} from "@/components/Hook0Card.vue";
+export {default as Hook0CardHeader} from "@/components/Hook0CardHeader.vue";
+export {default as Hook0CardFooter} from "@/components/Hook0CardFooter.vue";
+export {default as Hook0CardContent} from "@/components/Hook0CardContent.vue";
+export {default as Hook0CardContentLine} from "@/components/Hook0CardContentLine.vue";
 
 export default {
   install: (app: App) => {
