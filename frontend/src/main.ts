c1daab1262a536d895e8e3a598f6eac32efabc9c
François-Guillaume Ribreau <git@fgribreau.com>
2022-03-11 11:43:01 +0100

    feat: add footer + universal button

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -44,7 +44,23 @@ app.use(RouterOrgPlugin);
 app.component('Promised', Promised);
 
 // Vue - Hook0 own components
-//app.use(components); // TODO: finish or clean up
+import Hook0Alert from "@/components/Hook0Alert.vue";
+import Hook0Button from "@/components/Hook0Button.vue";
+import Hook0Input from "@/components/Hook0Input.vue";
+import Hook0Card from "@/components/Hook0Card.vue";
+import Hook0CardHeader from "@/components/Hook0CardHeader.vue";
+import Hook0CardFooter from "@/components/Hook0CardFooter.vue";
+import Hook0CardContent from "@/components/Hook0CardContent.vue";
+import Hook0CardContentLine from "@/components/Hook0CardContentLine.vue";
+
+app.component('Hook0Alert', Hook0Alert);
+app.component('Hook0Button', Hook0Button);
+app.component('Hook0Input', Hook0Input);
+app.component('Hook0Card', Hook0Card);
+app.component('Hook0CardHeader', Hook0CardHeader);
+app.component('Hook0CardFooter', Hook0CardFooter);
+app.component('Hook0CardContent', Hook0CardContent);
+app.component('Hook0CardContentLine', Hook0CardContentLine);
 
 // Mount the app
 app.mount('#app');
