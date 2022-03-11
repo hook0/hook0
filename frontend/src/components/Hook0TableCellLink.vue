245a124d9ffadb7b5914e60ba6a96266866196a3
François-Guillaume Ribreau <git@fgribreau.com>
2022-03-11 11:43:50 +0100

    feat(frontend): add Hook0Table and Hook0TableCellLink

---
+++ frontend/src/components/Hook0TableCellLink.vue
@@ -0,0 +1,26 @@
+<template>
+  <hook0-button
+    :to="params.colDef.cellRendererParams && params.colDef.cellRendererParams.href ? params.colDef.cellRendererParams.href(params.data) : undefined"
+  >
+    {{ params.value }}
+  </hook0-button>
+</template>
+
+
+<script lang="ts">
+import {Vue, Options} from 'vue-class-component';
+import {ICellRendererParams} from "@ag-grid-community/core";
+
+@Options({
+  name: 'hook0-table-cell-link',
+  inheritAttrs: false,
+  props: {},
+  components: {}
+})
+export default class Hook0Table extends Vue {
+  private params!: ICellRendererParams;
+};
+</script>
+<style>
+</style>
+
