4b669b5f10826c9fb40576a0b87131eb5991906a
François-Guillaume Ribreau <git@fgribreau.com>
2022-03-11 11:43:01 +0100

    feat: add footer + universal button

--- frontend/src/components/Hook0Button.vue
+++ frontend/src/components/Hook0Button.vue
@@ -1,8 +1,11 @@
 <template>
-  <button
-    :class="{ 'hook0-button': true, loading: loading }"
+  <a
+    v-bind="{ ...$props, ...$attrs }"
+    class="hook0-button"
+    :class="{ loading: loading }"
     @click="onClick($event)"
-    :disabled="loading"
+    :disabled="loading || disabled"
+    :href="_href"
   >
     <div class="hook0-button-left" v-if="hasSlot('left') && !loading">
       <slot name="left"></slot>
@@ -12,43 +15,94 @@
     <div class="hook0-button-right" v-if="hasSlot('right') && !loading">
       <slot name="right"></slot>
     </div>
-  </button>
+  </a>
 </template>
 
 <script lang="ts">
-import { Vue, Options } from 'vue-class-component';
-import Hook0Icon from '@/components/Hook0Icon.vue';
 
-@Options({
-  name: 'hook0-button',
+import Hook0Icon from '@/components/Hook0Icon.vue';
+import {RouteLocationRaw} from "vue-router";
+import {defineComponent, PropType} from 'vue'
+
+export default defineComponent({
+  components: {
+    Hook0Icon
+  },
+  // type inference enabled
   props: {
     loading: {
       type: Boolean,
       default: false,
     },
+    // helper to let the button go to a specified route
+    to: {
+      type: Object as PropType<RouteLocationRaw>,
+      required: false,
+    },
+    href: {
+      type: String,
+      required: false,
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+      required: false,
+    },
   },
-  components: {
-    Hook0Icon,
-  },
-})
-export default class Hook0Button extends Vue {
-  loading = false;
+  computed: {
+    _href(): undefined | string {
+      if (this.href) {
+        return this.href;
+      }
 
-  onClick(e: MouseEvent) {
-    if (this.loading) {
-      return;
+      if (!this.to) {
+        return undefined;
+      }
+
+      // @ts-ignore
+      const {href} = this.$router.resolve(this.to);
+      return href; // for accessibility
     }
-    this.$emit('click', e);
-  }
+  },
+  methods: {
+    onClick(e: MouseEvent) {
+      if (!this.href) {
+        e.preventDefault();
+      }
 
-  hasSlot(name = 'default'): boolean {
-    return !!this.$slots[name];
+      if (this.loading || this.disabled) {
+        // do nothing
+        return;
+      }
+
+      if (!this._href) {
+        // no href so bubble-up event
+        this.$emit('click', e);
+        return;
+      }
+
+      if (e.metaKey) {
+        // support for power-user that want to open links in another tab
+        window.open(this._href);
+        return true;
+      }
+
+      // @ts-ignore
+      this.$router.push(this.to).catch((err) => {
+        console.error(err);
+      });
+    },
+    hasSlot(name = 'default'): boolean {
+      return !!this.$slots[name];
+    }
   }
-};
+})
 </script>
 
 <style lang="scss" scoped>
 .hook0-button {
+  @apply select-none font-medium text-indigo-600 hover:text-indigo-500;
+
   &.primary {
     @apply inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500;
   }
@@ -58,7 +112,7 @@ export default class Hook0Button extends Vue {
   }
 
   &.danger {
-    @apply inline-flex items-center justify-center px-4 py-2 border border-transparent font-medium rounded-md text-red-700 bg-red-100 hover:bg-red-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:text-sm;
+    @apply inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md text-red-700 bg-red-100 hover:bg-red-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500;
   }
 
   &.white {
@@ -87,7 +141,7 @@ export default class Hook0Button extends Vue {
 }
 
 .hook0-button-left {
-  @apply -ml-1 mr-3 h-5 w-5;
+  @apply -ml-1 mb-1 h-5 w-5;
 }
 
 .hook0-button-right {
