e928f6a443dd139d6460f7443ca0bb8f7d3b6542
François-Guillaume Ribreau <git@fgribreau.com>
2022-05-29 10:32:43 +0200

    fix: convert timeouts axios's error to Problem

--- frontend/src/http.ts
+++ frontend/src/http.ts
@@ -1,21 +1,37 @@
-import {AxiosError, AxiosRequestConfig, AxiosResponse} from 'axios';
+import {Axios, AxiosError, AxiosRequestConfig, AxiosResponse} from 'axios';
 import axios from 'axios';
 import iam from './iam';
 import featureFlags from './feature-flags';
 import {definitions} from "@/types";
+import {identity} from "ramda";
 
 require('bluebird');
 
 function getAxios() {
-  return iam.getToken().then((jwt_token) =>
-    axios.create({
-      baseURL: featureFlags.getOrElse('API_ENDPOINT', process.env.VUE_APP_API_ENDPOINT),
-      timeout: 1000,
-      headers: {
-        Authorization: `Bearer ${jwt_token}`,
-      },
-      withCredentials: true,
-    })
+  return iam.getToken().then((jwt_token) => {
+      const client = axios.create({
+        baseURL: featureFlags.getOrElse('API_ENDPOINT', process.env.VUE_APP_API_ENDPOINT),
+        timeout: 1000,
+        headers: {
+          Authorization: `Bearer ${jwt_token}`,
+        },
+        withCredentials: false, // false in dev mode, true in staging/production mode
+
+      });
+
+      client.interceptors.response.use(identity, function (error: AxiosError) {
+        // Any status codes that falls outside the range of 2xx cause this function to trigger
+
+        // convert timeouts axios's error to Problem
+        if (isAxiosError(error) && String(error.message).includes('timeout of')) {
+          return Promise.reject(new ProblemFactory(0, "TimeoutExceeded", error.message, error.message));
+        }
+
+        return Promise.reject(error);
+      });
+
+      return client;
+    }
   );
 }
 
