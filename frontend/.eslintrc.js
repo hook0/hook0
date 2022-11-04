e82f7e36cb3ae5768f81b48f79094f7de0ad966d
François-Guillaume Ribreau <git@fgribreau.com>
2022-11-04 16:52:33 +0100

    fix: better navigation menu and bring back prettier

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -36,20 +36,86 @@ module.exports = {
       {
         varsIgnorePattern: '^[_].*',
         argsIgnorePattern: '^[_].*',
-      }
+      },
     ],
+  },
+
+  settings: {
+    'prettier-vue': {
+      // Settings for how to process Vue SFC Blocks
+      SFCBlocks: {
+        /**
+         * Use prettier to process `<template>` blocks or not
+         *
+         * If set to `false`, you may need to enable those vue rules that are disabled by `eslint-config-prettier`,
+         * because you need them to lint `<template>` blocks
+         *
+         * @default true
+         */
+        template: true,
+
+        /**
+         * Use prettier to process `<script>` blocks or not
+         *
+         * If set to `false`, you may need to enable those rules that are disabled by `eslint-config-prettier`,
+         * because you need them to lint `<script>` blocks
+         *
+         * @default true
+         */
+        script: true,
+
+        /**
+         * Use prettier to process `<style>` blocks or not
+         *
+         * @default true
+         */
+        style: true,
+
+        // Settings for how to process custom blocks
+        customBlocks: {
+          // Treat the `<docs>` block as a `.markdown` file
+          docs: { lang: 'markdown' },
+
+          // Treat the `<config>` block as a `.json` file
+          config: { lang: 'json' },
+
+          // Treat the `<module>` block as a `.js` file
+          module: { lang: 'js' },
+
+          // Ignore `<comments>` block (omit it or set it to `false` to ignore the block)
+          comments: false,
+
+          // Other custom blocks that are not listed here will be ignored
+        },
+      },
+
+      // Use prettierrc for prettier options
+      usePrettierrc: true,
+
+      // Set the options for `prettier.getFileInfo`.
+      // @see https://prettier.io/docs/en/api.html#prettiergetfileinfofilepath-options
+      fileInfoOptions: {
+        // Path to ignore file (default: `'.prettierignore'`)
+        // Notice that the ignore file is only used for this plugin
+        ignorePath: '.testignore',
+
+        // Process the files in `node_modules` or not (default: `false`)
+        withNodeModules: false,
+      },
+    },
   },
 
   extends: [
     'plugin:vue/vue3-essential',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    // 'plugin:prettier-vue/recommended', // TODO: find a way to make it work
+    'plugin:prettier-vue/recommended',
     'prettier',
   ],
 
   overrides: [
-    { // As types.ts is generated, we have to disable rules that it violates
+    {
+      // As types.ts is generated, we have to disable rules that it violates
       files: ['src/types.ts'],
       rules: {
         '@typescript-eslint/no-empty-interface': 'off',
@@ -61,5 +127,5 @@ module.exports = {
         '@typescript-eslint/no-unsafe-assignment': 'off',
       },
     },
-  ]
+  ],
 };
