389e0a855e1df71f2484b10f8d062df4ed705d56
François-Guillaume Ribreau <git@fgribreau.com>
2023-07-14 10:46:21 +0200

    chore(website): update deps

--- website/.ejsrc.js
+++ website/.ejsrc.js
@@ -1,10 +1,3 @@
-const locals = {};
-
-locals.meta = require('./data/meta');
-locals.social = require('./data/social');
-locals.prices = require('./data/prices');
-locals.features = require('./data/features')(locals);
-
 module.exports = {
-  "locals": locals
-}
+  locals: require('./data'),
+};
