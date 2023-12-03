c6981c4861f67a0c9f89b1afd3a3dfee11328534
David Sferruzza <david.sferruzza@gmail.com>
2023-12-03 12:48:22 +0100

    feat(output-worker): handle multiple request attempts concurrently

--- output-worker/src/monitoring.rs
+++ output-worker/src/monitoring.rs
@@ -1,6 +1,10 @@
-use log::trace;
+use anyhow::anyhow;
+use chrono::Utc;
+use log::{trace, warn};
 use reqwest::Url;
 use serde::Serialize;
+use std::time::Duration;
+use tokio::sync::mpsc::Receiver;
 
 #[derive(Debug, Clone, Serialize)]
 struct HearbeatBody<'a> {
@@ -8,11 +12,33 @@ struct HearbeatBody<'a> {
     worker_version: &'a str,
 }
 
-pub async fn send_heartbeat(
+pub async fn heartbeat_sender(
+    heartbeat_min_period: Duration,
     url: &Url,
+    mut rx: Receiver<u8>,
     worker_name: &str,
     worker_version: &str,
 ) -> anyhow::Result<()> {
+    let mut last_heartbeat = Utc::now() - heartbeat_min_period;
+
+    while let Some(unit_id) = rx.recv().await {
+        trace!("Ping received from unit {unit_id}");
+
+        if last_heartbeat + heartbeat_min_period <= Utc::now() {
+            trace!("Sending heartbeat...");
+            if let Err(e) = send_heartbeat(url, worker_name, worker_version).await {
+                warn!("Monitoring heartbeat failed: {e}");
+            };
+            last_heartbeat = Utc::now();
+        }
+    }
+
+    Err(anyhow!(
+        "Heartbeat sender task ended whereas it should never end"
+    ))
+}
+
+async fn send_heartbeat(url: &Url, worker_name: &str, worker_version: &str) -> anyhow::Result<()> {
     trace!("Sending monitoring heartbeat...");
     let res = reqwest::Client::new()
         .post(url.as_ref())
