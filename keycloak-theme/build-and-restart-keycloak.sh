6fdb9a59a294214fe2bc86ee278ec51ffb69ac40
François-Guillaume Ribreau <git@fgribreau.com>
2024-03-08 11:59:52 +0100

    fix(keycloak-teme): be ready for v13 and v14

--- keycloak-theme/build-and-restart-keycloak.sh
+++ keycloak-theme/build-and-restart-keycloak.sh
@@ -6,4 +6,4 @@ set -euo pipefail
 docker stop keycloak || true
 docker rm keycloak || true
 
-docker run --name keycloak -p 8080:8080 -e KEYCLOAK_ADMIN=admin -e KEYCLOAK_ADMIN_PASSWORD=admin -v /tmp/theme:/opt/keycloak/providers quay.io/keycloak/keycloak:18.0.0 start-dev
+docker run --name keycloak -p 8080:8080 -e KEYCLOAK_ADMIN=admin -e KEYCLOAK_ADMIN_PASSWORD=admin -v /tmp/theme:/opt/keycloak/providers quay.io/keycloak/keycloak:24.0.1 start-dev
