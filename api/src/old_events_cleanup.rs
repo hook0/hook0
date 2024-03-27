f56f840054466931f168da7a4429e342bc07e318
David Sferruzza <david.sferruzza@gmail.com>
2024-03-27 02:09:46 +0100

    refactor(api): old events cleanup

--- api/src/old_events_cleanup.rs
+++ api/src/old_events_cleanup.rs
@@ -1,26 +1,28 @@
 use actix::clock::sleep;
-use futures_util::TryStreamExt;
 use log::{debug, error, info, trace};
-use sqlx::postgres::types::PgInterval;
-use sqlx::{query, query_scalar, Acquire, PgPool, Postgres};
+use sqlx::{query, Acquire, PgPool, Postgres};
 use std::time::{Duration, Instant};
-use uuid::Uuid;
-
-use crate::quotas::{Quota, Quotas};
 
 const STARTUP_GRACE_PERIOD: Duration = Duration::from_secs(20);
 
 pub async fn periodically_clean_up_old_events(
     db: &PgPool,
-    quotas: &Quotas,
     period: Duration,
+    global_days_of_events_retention_limit: i32,
     grace_period_in_day: u16,
     delete: bool,
 ) {
     sleep(STARTUP_GRACE_PERIOD).await;
 
     loop {
-        if let Err(e) = clean_up_old_events(db, quotas, grace_period_in_day, delete).await {
+        if let Err(e) = clean_up_old_events_and_responses(
+            db,
+            global_days_of_events_retention_limit,
+            grace_period_in_day,
+            delete,
+        )
+        .await
+        {
             error!("Could not clean up old events: {e}");
         }
 
@@ -28,30 +30,25 @@ pub async fn periodically_clean_up_old_events(
     }
 }
 
-async fn clean_up_old_events(
+async fn clean_up_old_events_and_responses(
     db: &PgPool,
-    quotas: &Quotas,
+    global_days_of_events_retention_limit: i32,
     grace_period_in_day: u16,
     delete: bool,
 ) -> Result<(), sqlx::Error> {
     trace!("Start cleaning up old events...");
     let start = Instant::now();
     let mut tx = db.begin().await?;
-    let mut total_deleted_events = 0;
 
-    let mut applications = query_scalar!("SELECT application__id from event.application").fetch(db);
-    while let Some(application_id) = applications.try_next().await? {
-        let deleted_events = delete_old_events_for_application(
-            &mut tx,
-            quotas,
-            i32::from(grace_period_in_day),
-            &application_id,
-        )
-        .await?;
-        debug!("Found {deleted_events} old events to clean up for application '{application_id}'");
-        total_deleted_events += deleted_events;
-    }
+    debug!("Removing old events...");
+    let total_deleted_events = delete_old_events(
+        &mut *tx,
+        global_days_of_events_retention_limit,
+        i32::from(grace_period_in_day),
+    )
+    .await?;
 
+    debug!("Removing dangling responses...");
     let total_dangling_responses = delete_dangling_responses(&mut *tx).await?;
 
     if delete {
@@ -64,31 +61,32 @@ async fn clean_up_old_events(
     Ok(())
 }
 
-async fn delete_old_events_for_application<'a, A: Acquire<'a, Database = Postgres>>(
+async fn delete_old_events<'a, A: Acquire<'a, Database = Postgres>>(
     db: A,
-    quotas: &Quotas,
+    global_days_of_events_retention_limit: i32,
     grace_period_in_day: i32,
-    application_id: &Uuid,
 ) -> Result<u64, sqlx::Error> {
     let mut db = db.acquire().await?;
 
-    let retention_in_days = quotas
-        .get_limit_for_application(&mut *db, Quota::DaysOfEventsRetention, application_id)
-        .await?;
-    trace!("Event retention for application '{application_id}' is {retention_in_days} days (plus {grace_period_in_day} days grace period)");
-
-    let retention = PgInterval {
-        days: retention_in_days + grace_period_in_day,
-        ..Default::default()
-    };
     let res = query!(
         "
+            WITH retention AS (
+                SELECT a.application__id, MAKE_INTERVAL(days => COALESCE(LEAST(a.days_of_events_retention_limit, p.days_of_events_retention_limit), $1) + $2) AS events_retention_limit
+                FROM event.application AS a
+                INNER JOIN iam.organization AS o ON o.organization__id = a.organization__id
+                LEFT JOIN pricing.price AS pr ON pr.price__id = o.price__id
+                LEFT JOIN pricing.plan AS p ON p.plan__id = pr.plan__id
+            )
             DELETE FROM event.event
-            WHERE application__id = $1
-                AND received_at + $2 < statement_timestamp()
+            WHERE event__id IN (
+                SELECT e.event__id
+                FROM event.event AS e
+                INNER JOIN retention AS r ON r.application__id = e.application__id
+                WHERE e.received_at + r.events_retention_limit < statement_timestamp()
+            );
         ",
-        application_id,
-        retention,
+        global_days_of_events_retention_limit,
+        grace_period_in_day,
     )
     .execute(&mut *db)
     .await?;
