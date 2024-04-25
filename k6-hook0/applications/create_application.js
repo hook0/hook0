4209979fca569b73902da83f8494c6d2f2c93bfc
Thomas Tartrau <thomas.tartrau@ecoles-epsi.net>
2024-04-25 13:02:35 +0200

    feat(k6): scenario fully created / waiting for David's review

--- k6-hook0/applications/create_application.js
+++ k6-hook0/applications/create_application.js
@@ -17,7 +17,9 @@ export default function(baseUrl, organizationId, masterApiKey) {
     const res = http.post(url, payload, params);
     if(!check(res, {
         'Application created': (r) => r.status === 201 && r.body && r.body.includes('organization_id') && r.body.includes('name') && r.body.includes('application_id'),
-    })) return null;
+    })) {
+        return null;
+    }
 
     return JSON.parse(res.body).application_id;
 }
--- k6-hook0/main.js
+++ k6-hook0/main.js
@@ -15,15 +15,14 @@ export const config = getEnvironmentVariables();
 export const options = {
     vus: config.vus,
     duration: config.duration,
-    maxDuration: config.max_duration,
+    //maxDuration: config.maxDuration,
 }
 
 function isNotNull(value) {
     return value && value !== null;
 }
 
-// Setup
-export function setup() {
+function scenario_1() {
     let createUserBody = create_user(config.hostname);
     if (!isNotNull(createUserBody)) {
         console.error("Failed to create user");
@@ -63,8 +62,8 @@ export function setup() {
         return;
     }
     let subscription_1_id = subscription_1.subscription_id;
-    let label_key = subscription_1.label_key;
-    let label_value = subscription_1.label_value;
+    let label_1_key = subscription_1.label_key;
+    let label_1_value = subscription_1.label_value;
 
 
     let subscription_2 = create_subscription(config.hostname, application_secret, application_id, [event_type_1], config.targetUrl, "all", "yes");
@@ -72,16 +71,26 @@ export function setup() {
         console.error("Failed to create subscription");
         return;
     }
+    let subscription_2_id = subscription_2.subscription_id;
+    let label_2_key = subscription_2.label_key;
+    let label_2_value = subscription_2.label_value;
 
-    let event_1 = create_event(application_secret, config.hostname, application_id, event_type_1);
+    let event_1 = create_event(application_secret, config.hostname, application_id, event_type_1, {[label_1_key]: label_1_value});
     if (!isNotNull(event_1)) {
         console.error("Failed to create event 1");
         return;
     }
 
-    let event_2 = create_event(application_secret, config.hostname, application_id, event_type_2);
+    let event_2 = create_event(application_secret, config.hostname, application_id, event_type_2, {[label_2_key]: label_2_value});
     if (!isNotNull(event_2)) {
         console.error("Failed to create event 2");
+        return;
+    }
+
+    // Event 3 shoudn't be delivered
+    let event_3 = create_event(application_secret, config.hostname, application_id, event_type_1, {'test': 'test'});
+    if (!isNotNull(event_3)) {
+        console.error("Failed to create event 3");
         return;
     }
 
@@ -104,7 +113,19 @@ export function setup() {
         console.error("Expected to find 1 request attempts for event 2 | Found: " + request_attempts_2.length);
         return;
     }
+
+    // Event 3 soulnd't be received by any subscription so the length should be 0
+    let request_attempts_3 = list_requests_attempts(config.hostname, application_secret, application_id, event_3);
+    if (!isNotNull(request_attempts_3)) {
+        console.error("Failed to list request attempts 3");
+        return;
+    }
+    if(request_attempts_3.length !== 0) {
+        console.error("Expected to find 0 request attempts for event 3 | Found: " + request_attempts_3.length);
+        return;
+    }
 }
 
 export default function () {
+    scenario_1();
 }
