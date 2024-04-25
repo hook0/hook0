2392f43e4be3af2e40cfa51a7b34be1333930c87
Thomas Tartrau <thomas.tartrau@ecoles-epsi.net>
2024-04-25 13:02:35 +0200

    feat(k6): scenario fully created / waiting for David's review

--- k6-hook0/event_types/create_event_type.js
+++ k6-hook0/event_types/create_event_type.js
@@ -22,7 +22,9 @@ export default function (baseUrl, application_secret, application_id) {
 
   if(!check(res, {
     "Create event type is successful": (r) => r.status === 201 && r.body && r.body.includes('resource_type_name') && r.body.includes('service_name') && r.body.includes('verb_name') && r.body.includes('event_type_name'),
-  })) return null;
+  })) {
+    return null;
+  }
 
   return JSON.parse(res.body).event_type_name;
 }
