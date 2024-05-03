0c1429060f15651c678de97fc2c15fb140db1169
Thomas Tartrau <thomas.tartrau@ecoles-epsi.net>
2024-05-03 15:28:11 +0200

    feat(frontend): tentative to add notifications system into hook0 project

---
+++ frontend/src/components/Hook0Notifications.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { Notivue, Notification, NotificationProgress } from 'notivue';
+</script>
+
+<template>
+  <Notivue v-slot="item">
+    <Notification :item="item" />
+    <NotificationProgress :item="item" />
+  </Notivue>
+</template>
