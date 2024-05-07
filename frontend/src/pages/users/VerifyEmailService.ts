97baeb4a81fde74d223f086d59ca6073b9ccfe87
Thomas Tartrau <thomas.tartrau@ecoles-epsi.net>
2024-05-07 11:24:33 +0200

    feat(frontend): Add and modify UserSettings to the new iam + add frontend change password

--- frontend/src/pages/users/VerifyEmailService.ts
+++
@@ -1,9 +0,0 @@
-import http, { handleError, Problem } from '@/http';
-import { AxiosError, AxiosResponse } from 'axios';
-
-export function verifyEmail(token: string): Promise<void> {
-  return http.post(`/auth/verify-email`, { token }).then(
-    (res: AxiosResponse<void>) => res.data,
-    (err: AxiosError<AxiosResponse<Problem>>) => Promise.reject(handleError(err))
-  );
-}
