9dadb1501a0dd87a642012e1b72d3ff7b0be3e50
Thomas Tartrau <thomas.tartrau@ecoles-epsi.net>
2024-05-08 12:52:19 +0200

    fix(frontend): some fix + start implement for reset password frontend

--- frontend/src/pages/users/UsersServices.ts
+++ frontend/src/pages/users/UsersServices.ts
@@ -1,7 +1,7 @@
 import http, { handleError, Problem } from '@/http.ts';
 import { AxiosError, AxiosResponse } from 'axios';
 
-export function deleteUser(): Promise<void> {
+export async function deleteUser(): Promise<void> {
   return Promise.reject({
     id: 'ComingSoon',
     title: 'Not implemented yet',
@@ -22,8 +22,16 @@ export async function changePassword(new_password: string): Promise<void> {
     );
 }
 
+// Check to put in async or not ?
 export function verifyEmail(token: string): Promise<void> {
   return http.unauthenticated.post(`/auth/verify-email`, { token }).then(
+    (res: AxiosResponse<void>) => res.data,
+    (err: AxiosError<AxiosResponse<Problem>>) => Promise.reject(handleError(err))
+  );
+}
+
+export async function beginResetPassword(email: string): Promise<void> {
+  return http.unauthenticated.post(`/auth/begin-reset-password`, { email }).then(
     (res: AxiosResponse<void>) => res.data,
     (err: AxiosError<AxiosResponse<Problem>>) => Promise.reject(handleError(err))
   );
