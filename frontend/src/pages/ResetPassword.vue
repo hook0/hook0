0bdf99c7f229063619f9868ff4888fa1cc0749d0
Thomas Tartrau <thomas.tartrau@ecoles-epsi.net>
2024-05-08 12:52:19 +0200

    fix(frontend): some fix + start implement for reset password frontend

---
+++ frontend/src/pages/ResetPassword.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import Hook0Card from '@/components/Hook0Card.vue';
+import Hook0CardHeader from '@/components/Hook0CardHeader.vue';
+</script>
+
+<template>
+  <Hook0Card>
+    <Hook0CardHeader>
+      <template #header>Coming soon...</template>
+      <template #subtitle
+        >Hook0 API is complete and production ready but the UI is still a work in progress...
+      </template>
+    </Hook0CardHeader>
+  </Hook0Card>
+</template>
