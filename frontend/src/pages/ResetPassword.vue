62da628391bbde5361d814cd6428bf69df3ff0df
Thomas Tartrau <thomas.tartrau@ecoles-epsi.net>
2024-05-08 13:49:36 +0200

    feat(frontend): finish begin reset password && reset password frontend part

--- frontend/src/pages/ResetPassword.vue
+++ frontend/src/pages/ResetPassword.vue
@@ -4,7 +4,7 @@ import Hook0CardHeader from '@/components/Hook0CardHeader.vue';
 import Hook0CardContent from '@/components/Hook0CardContent.vue';
 import Hook0CardContentLine from '@/components/Hook0CardContentLine.vue';
 import Hook0Input from '@/components/Hook0Input.vue';
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
 import Hook0Button from '@/components/Hook0Button.vue';
 import Hook0CardFooter from '@/components/Hook0CardFooter.vue';
 import { push } from 'notivue';
@@ -13,6 +13,15 @@ import { handleError, Problem } from '@/http.ts';
 import { resetPassword } from '@/pages/users/UsersServices.ts';
 import { routes } from '@/routes.ts';
 import router from '@/router.ts';
+import Hook0Alert from '@/components/Hook0Alert.vue';
+import { Alert } from '@/components/Hook0Alert.ts';
+
+const alert = ref<Alert>({
+  visible: false,
+  type: 'alert',
+  title: '',
+  description: '',
+});
 
 const token = ref('');
 const new_password = ref('');
@@ -44,33 +53,71 @@ async function submit() {
 
 function displayError(err: Problem) {
   console.error(err);
-  let options = {
-    title: err.title,
-    message: err.detail,
-    duration: 5000,
-  };
-  err.status >= 500 ? push.error(options) : push.warning(options);
+  alert.value.visible = true;
+
+  alert.value.type = err.status >= 500 ? 'alert' : 'warning';
+  alert.value.title = err.title;
+  alert.value.description = err.detail;
 }
+
+function _onLoad() {
+  token.value = router.currentRoute.value.query.token as string;
+  if (!token.value) {
+    displayError({
+      id: 'InvalidToken',
+      status: 400,
+      title: 'Invalid token',
+      detail: 'Token is required to reset password',
+    });
+    return;
+  }
+}
+
+onMounted(() => {
+  _onLoad();
+});
 </script>
 
 <template>
-  <form @submit="submit">
+  <Hook0CardContent v-if="alert.visible">
+    <Hook0Alert
+      :type="alert.type"
+      :title="alert.title"
+      :description="alert.description"
+    ></Hook0Alert>
+  </Hook0CardContent>
+  <form v-else @submit="submit">
     <Hook0Card>
       <Hook0CardHeader>
-        <template #header> Reset my account password </template>
-        <template #subtitle>
-          <div class="text-sm text-gray-500">
-            For security reasons, we need to verify your identity. We've sent a mail to your email
-            address. Please click on the link in the email to reset your password.
-          </div>
-        </template>
+        <template #header> Change account password </template>
       </Hook0CardHeader>
       <Hook0CardContent>
         <Hook0CardContentLine>
-          <template #label> Email </template>
+          <template #label> New password </template>
           <template #content>
             <div class="flex flex-row">
-              <Hook0Input v-model="email" type="email" class="w-full" placeholder="Email" required>
+              <Hook0Input
+                v-model="new_password"
+                type="password"
+                class="w-full"
+                placeholder="New password"
+                required
+              >
+              </Hook0Input>
+            </div>
+          </template>
+        </Hook0CardContentLine>
+        <Hook0CardContentLine>
+          <template #label> Confirm new password </template>
+          <template #content>
+            <div class="flex flex-row">
+              <Hook0Input
+                v-model="confirm_new_password"
+                type="password"
+                class="w-full"
+                placeholder="Confirm new password"
+                required
+              >
               </Hook0Input>
             </div>
           </template>
