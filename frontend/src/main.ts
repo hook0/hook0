8c215e2e070845f72a2a8138b86c059bdf8dbe7b
Thomas Tartrau <thomas.tartrau@ecoles-epsi.net>
2024-05-13 14:32:58 +0200

    feat(frontend): finish the part for attenuate biscuit token with some params

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -61,6 +61,7 @@ app.use(AuthPlugin);
 // Notivue
 import 'notivue/notification.css'; // Only needed if using built-in notifications
 import 'notivue/animations.css'; // Only needed if using built-in animations
+import 'notivue/notification-progress.css';
 
 const notivue = createNotivue({
   position: 'top-right',
