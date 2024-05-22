bef16d14b6991ccf5d085c693c71b241d68b8aa5
Thomas Tartrau <thomas.tartrau@ecoles-epsi.net>
2024-05-22 10:33:47 +0200

    feat(k6): implement k6 for the new iam version (v2) with a scenario (check README.md)

---
+++ k6-hook0/scenario-1/events/list_request_attempt.js
@@ -0,0 +1,22 @@
+import http from 'k6/http';
+import { check } from 'k6';
+
+export default function (base_url, service_token, application_id, event_id) {
+    let res = http.get(
+        `${base_url}api/v1/request_attempts/?application_id=${application_id}&event_id=${event_id}`,
+        {
+            headers: {
+                Authorization: `Bearer ${service_token}`,
+                accept: 'application/json',
+            },
+        },
+    );
+
+    if(!check(res, {
+        'List request attempts': (r) => r.status === 200 && r.body,
+    })) {
+        return null;
+    }
+
+    return JSON.parse(res.body);
+}
