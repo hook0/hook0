88b1fd26e239e1f0694a19744d9a2bb34d64b339
Thomas Tartrau <thomas.tartrau@ecoles-epsi.net>
2024-05-22 10:33:47 +0200

    feat(k6): implement k6 for the new iam version (v2) with a scenario (check README.md)

---
+++ k6-hook0/scenario-1/applications/create_application.js
@@ -0,0 +1,25 @@
+import http from 'k6/http';
+import { check } from 'k6';
+
+export default function(baseUrl, organizationId, serviceToken) {
+    const url = `${baseUrl}api/v1/applications/`;
+    const payload = JSON.stringify({
+        name: 'test_k6',
+        organization_id: organizationId, // Ensure this value is not undefined or null
+    });
+    const params = {
+        headers: {
+            'Authorization': `Bearer ${serviceToken}`,
+            'accept': 'application/json',
+            'content-type': 'application/json',
+        },
+    };
+    const res = http.post(url, payload, params);
+    if(!check(res, {
+        'Application created': (r) => r.status === 201 && r.body && r.body.includes('organization_id') && r.body.includes('name') && r.body.includes('application_id'),
+    })) {
+        return null;
+    }
+
+    return JSON.parse(res.body).application_id;
+}
---
+++ k6-hook0/scenario-1/events/send_event.js
@@ -0,0 +1,31 @@
+import http from 'k6/http';
+import { check } from 'k6';
+import { uuidv4 } from 'https://jslib.k6.io/k6-utils/1.4.0/index.js';
+
+export default function (service_token, base_url, application_id, event_type, labels) {
+    let url = base_url + 'api/v1/event/';
+    let headers = {
+        'Authorization': `Bearer ${service_token}`,
+        'accept': 'application/json',
+        'content-type': 'application/json'
+    };
+    let payload = {
+        "labels": labels,
+        "application_id": application_id,
+        "event_id": uuidv4(),
+        "event_type": event_type,
+        "occurred_at": "2022-11-04T16:12:58Z",
+        "payload_content_type": "application/json",
+        "payload": "{\"test_k6\": true}",
+    };
+
+    let res = http.post(url, JSON.stringify(payload), {headers: headers});
+
+    if(!check(res, {
+        'Ingest event': (r) => r.status === 201 && r.body && r.body.includes('event_id') && r.body.includes('received_at'),
+    })) {
+        return null;
+    }
+
+    return JSON.parse(res.body).event_id;
+}
