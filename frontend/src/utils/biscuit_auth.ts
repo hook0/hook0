0a9c096bf6334f2f98e990c9859f76add9b1e59a
David Sferruzza <david.sferruzza@gmail.com>
2024-06-21 11:52:28 +0200

    refactor(frontend): remove non-working WASM check

--- frontend/src/utils/biscuit_auth.ts
+++ frontend/src/utils/biscuit_auth.ts
@@ -1,8 +1,6 @@
 import { Biscuit, block, PublicKey } from '@biscuit-auth/biscuit-wasm';
 import http, { handleError, Problem, UUID } from '@/http.ts';
 import { parse } from 'uuid';
-import { push } from 'notivue';
-import { App, Plugin } from 'vue';
 import { AxiosError, AxiosResponse } from 'axios';
 import { components } from '@/types.ts';
 
@@ -45,29 +43,3 @@ export function attenuateBiscuit(
   }
   return biscuit;
 }
-
-export const checkWebAssembly: Plugin = {
-  install(_app: App) {
-    try {
-      if (typeof WebAssembly === 'object' && typeof WebAssembly.instantiate === 'function') {
-        const module = new WebAssembly.Module(
-          Uint8Array.of(0x0, 0x61, 0x73, 0x6d, 0x01, 0x00, 0x00, 0x00)
-        );
-        if (
-          !(module instanceof WebAssembly.Module) ||
-          !(new WebAssembly.Instance(module) instanceof WebAssembly.Instance)
-        ) {
-          throw new Error();
-        }
-      } else {
-        throw new Error();
-      }
-    } catch (e) {
-      push.error({
-        title: 'WebAssembly Unsupported',
-        message:
-          'Your browser does not support WebAssembly. You need to enable it on your browser or install a browser that supports it.',
-      });
-    }
-  },
-};
