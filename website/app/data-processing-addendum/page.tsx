83a64e546a1da3693632d0c635f0173e72428113
ThomasTartrau <thomas.tartrau@ecoles-epsi.net>
2024-06-21 19:48:39 +0200

    fix(website): pipeline fix

--- website/app/data-processing-addendum/page.tsx
+++ website/app/data-processing-addendum/page.tsx
@@ -1,7 +1,330 @@
-export default function DataProcessingAddendumPage({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <section id="data-processing-addendum">{children}</section>;
+export default function DataProcessingAddendumPage() {
+  return (
+    <section id="data-processing-addendum">
+      <div className="mt-40 relative py-1 overflow-hidden">
+        <div className="relative px-4 sm:px-6 lg:px-8">
+          <div className="text-lg max-w-prose prose prose-lg mx-auto">
+            <h1>
+              <span className="block text-center text-indigo-500 text-2xl font-bold tracking-wide uppercase mb-10">
+                Data Processing Addendum
+              </span>
+            </h1>
+
+            <div>
+              <div className="section haze wf-section">
+                <div className="wrapper">
+                  <div className="intro">
+                    <div className="mb-8 text-xl font-semibold">
+                      Last Update: November 14th, 2021
+                    </div>
+                  </div>
+                  <div className="w-layout-grid main-grid">
+                    <div className="text-slate-300">
+                      <p className="mb-4">
+                        This GDPR Data Processing Addendum (“DPA”) forms part of
+                        the Master Services Agreement or Terms of Use available
+                        at{" "}
+                        <a
+                          className="text-indigo-400 underline"
+                          href="terms.ejs"
+                        >
+                          www.hook0.com/terms.html
+                        </a>
+                        or such other location as the Terms of Use may be posted
+                        from time to time (as applicable, the “Agreement”),
+                        entered into by and between the Customer and Hook0.
+                        (“Hook0”), pursuant to which Customer has accessed
+                        Hook0’s Application Services as defined in the
+                        applicable Agreement. The purpose of this DPA is to
+                        reflect the parties’ agreement with regard to the
+                        processing of personal data in accordance with the
+                        requirements of Data Protection Legislation as defined
+                        below.
+                      </p>
+
+                      <p className="mb-4">
+                        If the Customer entity entering into this DPA has
+                        executed an order form or statement of work with Hook0
+                        pursuant to the Agreement (an “Ordering Document”), but
+                        is not itself a party to the Agreement, this DPA is an
+                        addendum to that Ordering Document and applicable
+                        renewal Ordering Documents. If the Customer entity
+                        entering into this DPA is neither a party to an Ordering
+                        Document nor the Agreement, this DPA is not valid and is
+                        not legally binding. Such entity should request that the
+                        Customer entity that is a party to the Agreement
+                        executes this DPA.
+                      </p>
+
+                      <p className="mb-4">
+                        This DPA shall not replace or supersede any agreement or
+                        addendum relating to processing of personal data
+                        negotiated by Customer and referenced in the Agreement,
+                        and any such individually negotiated agreement or
+                        addendum shall apply instead of this DPA.
+                      </p>
+
+                      <p className="mb-8">
+                        In the course of providing the Application Services to
+                        Customer pursuant to the Agreement, Hook0 may process
+                        personal data on behalf of Customer. Hook0 agrees to
+                        comply with the following provisions with respect to any
+                        personal data submitted by or for Customer to the
+                        Application Services or collected and processed by or
+                        for Customer through the Application Services. Any
+                        capitalized but undefined terms herein shall have the
+                        meaning set forth in the Agreement.
+                      </p>
+
+                      <p className="mb-6 text-xl text-white font-semibold">
+                        Data Processing Terms
+                      </p>
+
+                      <p className="mb-4">
+                        In this DPA, “Data Protection Legislation” means
+                        European Directives 95/46/EC and 2002/58/EC (as amended
+                        by Directive 2009/136/EC) and any legislation and/or
+                        regulation implementing or made pursuant to them, or
+                        which amends, replaces, re-enacts or consolidates any of
+                        them (including the General Data Protection Regulation
+                        (Regulation (EU) 2016/279)), and all other applicable
+                        laws relating to processing of personal data and privacy
+                        that may exist in any relevant jurisdiction.
+                      </p>
+
+                      <p className="mb-4">
+                        “data controller”, “data processor”, “data subject”,
+                        “personal data”, “processing”, and “appropriate
+                        technical and organisational measures” shall be
+                        interpreted in accordance with applicable Data
+                        Protection Legislation;
+                      </p>
+
+                      <p className="mb-4">
+                        The parties agree that Customer is the data controller
+                        and that Hook0 is its data processor in relation to
+                        personal data that is processed in the course of
+                        providing the Application Services. Customer shall
+                        comply at all times with Data Protection Legislation in
+                        respect of all personal data it provided to Hook0
+                        pursuant to the Agreement.
+                      </p>
+
+                      <p className="mb-4">
+                        The subject-matter of the data processing covered by
+                        this DPA is the Application Services ordered by Customer
+                        either through Hook0’s website or through an Ordering
+                        Document and provided by Hook0 to Customer via
+                        www.hook0.com, or as additionally described in the
+                        Agreement or the DPA. The processing will be carried out
+                        until the term of Customer’s ordering of the Application
+                        Services ceases. Further details of the data processing
+                        are set out in Annex 1 hereto.
+                      </p>
+
+                      <p className="mb-4">
+                        In respect of personal data processed in the course of
+                        providing the Application Services, Hook0:
+                      </p>
+
+                      <ol className="list-decimal">
+                        <li className="mb-4 ml-8">
+                          shall process the personal data only in accordance
+                          with the documented instructions from Customer (as set
+                          out in this DPA or the Agreement or as otherwise
+                          notified by Customer to Hook0 (from time to time) If
+                          Hook0 is required to process the personal data for any
+                          other purpose provided by applicable law to which it
+                          is subject, Hook0 will inform Customer of such
+                          requirement prior to the processing unless that law
+                          prohibits this on important grounds of public
+                          interest;
+                        </li>
+                        <li className="mb-4 ml-8">
+                          shall notify Customer without undue delay if, in
+                          Hook0’s opinion, an instruction for the processing of
+                          personal data given by Customer infringes applicable
+                          Data Protection Legislation;
+                        </li>
+                        <li className="mb-4 ml-8">
+                          shall implement and maintain appropriate technical and
+                          organisational measures designed to protect the
+                          personal data against unauthorised or unlawful
+                          processing and against accidental loss, destruction,
+                          damage, theft, alteration or disclosure. These
+                          measures shall be appropriate to the harm which might
+                          result from any unauthorised or unlawful processing,
+                          accidental loss, destruction, damage or theft of the
+                          personal data and having regard to the nature of the
+                          personal data which is to be protected;
+                        </li>
+                        <li className="mb-4 ml-8">
+                          may hire other companies to provide limited services
+                          on its behalf, provided that Hook0 complies with the
+                          provisions of this Clause. Any such subcontractors
+                          will be permitted to process personal data only to
+                          deliver the services Hook0 has retained them to
+                          provide, and they shall be prohibited from using
+                          personal data for any other purpose. Hook0 remains
+                          responsible for its subcontractors’ compliance with
+                          the obligations of this DPA. Any subcontractors to
+                          whom Hook0 transfers personal data will have entered
+                          into written agreements with Hook0 requiring that the
+                          subcontractor abide by terms substantially similar to
+                          this DPA. A list of subcontractors is available to the
+                          Customer{" "}
+                          <a
+                            className="text-indigo-400 underline"
+                            href="gdpr-subprocessors.ejs"
+                          >
+                            here
+                          </a>
+                          . If Customer requires prior notification of any
+                          updates to the list of subprocessors, Customer can
+                          request such notification in writing by emailing
+                          support@hook0.com. Hook0 will update the list within
+                          thirty (30) days of any such notification if Customer
+                          does not legitimately object within that timeframe.
+                          Legitimate objections must contain reasonable and
+                          documented grounds relating to a subcontractor’s
+                          non-compliance with applicable Data Protection
+                          Legislation. If, in Hook0’s reasonable opinion, such
+                          objections are legitimate, the Customer may, by
+                          providing written notice to Hook0, terminate the
+                          Agreement.
+                        </li>
+                        <li className="mb-4 ml-8">
+                          shall ensure that all Hook0 personnel required to
+                          access the personal data are informed of the
+                          confidential nature of the personal data and comply
+                          with the obligations sets out in this Clause;
+                        </li>
+                        <li className="mb-4 ml-8">
+                          at the Customer’s request and cost (and insofar as is
+                          possible), shall assist the Customer by implementing
+                          appropriate and reasonable technical and
+                          organisational measures to assist with the Customer’s
+                          obligation to respond to requests from data subjects
+                          under Data Protection Legislation (including requests
+                          for information relating to the processing, and
+                          requests relating to access, rectification, erasure or
+                          portability of the personal data) provided that Hook0
+                          reserves the right to reimbursement from Customer for
+                          the reasonable cost of any time, expenditures or fees
+                          incurred in connection with such assistance;
+                        </li>
+                        <li className="mb-4 ml-8">
+                          when the General Data Protection Regulation
+                          (Regulation (EU) 2016/279) comes into effect, shall
+                          take reasonable steps at the Customer’s request and
+                          cost to assist Customer in meeting Customer’s
+                          obligations under Article 32 to 36 of that regulation
+                          taking into account the nature of the processing under
+                          this DPA, provided that Hook0 reserves the right to
+                          reimbursement from Customer for the reasonable cost of
+                          any time, expenditures or fees incurred in connection
+                          with such assistance;
+                        </li>
+                        <li className="mb-4 ml-8">
+                          at the end of the applicable term of the Application
+                          Services, upon Customer’s request, shall securely
+                          destroy or return such personal data to Customer;
+                        </li>
+                        <li className="mb-4 ml-8">
+                          may transfer personal data from the EEA to the US for
+                          the purposes of this DPA pursuant to the EU-US Privacy
+                          Shield provided that Hook0 maintains its certification
+                          under the EU-US Privacy Shield;
+                        </li>
+                        <li className="mb-4 ml-8">
+                          shall allow Customer and its respective auditors or
+                          authorized agents to conduct audits or inspections
+                          during the term of the Agreement, which shall include
+                          providing reasonable access to the premises, resources
+                          and personnel used by Hook0 in connection with the
+                          provision of the Application Services, and provide all
+                          reasonable assistance in order to assist Customer in
+                          exercising its audit rights under this Clause. The
+                          purposes of an audit pursuant to this Clause include
+                          to verify that Hook0 is processing personal data in
+                          accordance with its obligations under the DPA and
+                          applicable Data Protection Legislation.
+                          Notwithstanding the foregoing, such audit shall
+                          consist solely of: (i) the provision by Hook0 of
+                          written information (including, without limitation,
+                          questionnaires and information about security
+                          policies) that may include information relating to
+                          subcontractors; and (ii) interviews with Hook0’s IT
+                          personnel. Such audit may be carried out by Customer
+                          or an inspection body composed of independent members
+                          and in possession of the required professional
+                          qualifications bound by a duty of confidentiality. For
+                          the avoidance of doubt no access to any part of
+                          Hook0’s IT system, data hosting sites or centers, or
+                          infrastructure will be permitted;
+                        </li>
+                        <li className="mb-4 ml-8">
+                          If Hook0 becomes aware of any accidental, unauthorised
+                          or unlawful destruction, loss, alteration, or
+                          disclosure of, or access to the personal data that is
+                          processed by Hook0 in the course of providing the
+                          Application Services (an “Incident”) under the
+                          Agreement it shall without undue delay notify Customer
+                          and provide Customer (as soon as possible) with a
+                          description of the Incident as well as periodic
+                          updates to information about the Incident, including
+                          its impact on Customer Content. Hook0 shall
+                          additionally take action to investigate the Incident
+                          and reasonably prevent or mitigate the effects of the
+                          Incident;
+                        </li>
+                        <li className="mb-8 ml-8">
+                          Hook0 shall provide information requested by Customer
+                          to demonstrate compliance with the obligations set out
+                          in this DPA.
+                        </li>
+                      </ol>
+
+                      <h3 className="text-xl font-semibold text-white mb-4">
+                        Types of Personal Data:
+                      </h3>
+                      <ul className="list-disc ml-8 mb-4">
+                        <li className="mb-2">URL</li>
+                        <li className="mb-2">
+                          City, Region, Country, parsed from IP
+                        </li>
+                        <li className="mb-2">Browser</li>
+                        <li>Referrer</li>
+                      </ul>
+
+                      <p className="mb-8">
+                        Additional details regarding what information Customer
+                        may send to Hook0 can be asked here: support@hook0.com.
+                      </p>
+
+                      <h3 className="text-xl font-semibold text-white mb-4">
+                        Categories of Data Subjects
+                      </h3>
+                      <p className="mb-8">
+                        Users of the Customers web and mobile applications,
+                        reports, chat bots and everything that display Hook0.
+                      </p>
+
+                      <h3 className="text-xl font-semibold text-white mb-4">
+                        Processing Activities
+                      </h3>
+                      <p className="mb-4">
+                        The provision of Application Services by Hook0 to
+                        Customer.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
