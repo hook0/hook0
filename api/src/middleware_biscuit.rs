592d40e9fffe4b56830f0f54ac4658393664ef64
David Sferruzza <david.sferruzza@gmail.com>
2024-06-21 11:49:03 +0200

    fix(api): reject revoked application secrets

--- api/src/middleware_biscuit.rs
+++ api/src/middleware_biscuit.rs
@@ -190,6 +190,7 @@ where
                                                         FROM event.application_secret AS s
                                                         INNER JOIN event.application AS a ON a.application__id = s.application__id
                                                         WHERE s.token = $1
+                                                            and s.deleted_at IS NULL
                                                     ",
                                                     application_secret_token,
                                                 )
