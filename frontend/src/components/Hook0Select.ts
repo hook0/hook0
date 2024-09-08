97831e22cafc1ace51bd3d94170e6eda5f05129f
David Sferruzza <david.sferruzza@gmail.com>
2024-09-08 16:05:47 +0000

    Fix Hook0Select bug & improve subscriptions target validation

--- frontend/src/components/Hook0Select.ts
+++ frontend/src/components/Hook0Select.ts
@@ -7,3 +7,31 @@ export type Hook0SelectGroupedOption = {
   label?: string;
   options: Array<Hook0SelectSingleOption>;
 };
+
+export function isValidOption(
+  options: Array<Hook0SelectSingleOption | Hook0SelectGroupedOption>,
+  value?: string | null
+): boolean {
+  return options.some((o) => {
+    if ('value' in o) {
+      return o.value === value;
+    } else {
+      return isValidOption(o.options, value);
+    }
+  });
+}
+
+export function firstValue(
+  options: Array<Hook0SelectSingleOption | Hook0SelectGroupedOption>
+): string {
+  const first = options[0];
+  if (first) {
+    if ('value' in first) {
+      return first.value;
+    } else {
+      return firstValue(first.options);
+    }
+  } else {
+    return '';
+  }
+}
