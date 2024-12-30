b88191e59e105271c7861cca94b55b918e0837fd
David Sferruzza <david.sferruzza@gmail.com>
2024-12-30 18:32:35 +0100

    chore: update dependencies

--- frontend/src/components/Hook0TableCellIcon.vue
+++ frontend/src/components/Hook0TableCellIcon.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ICellRendererParams } from '@ag-grid-community/core';
+import { ICellRendererParams } from 'ag-grid-community';
 
 import Hook0Icon from '@/components/Hook0Icon.vue';
 
