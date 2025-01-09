ac20df0864ddb9900d0a32afa81b6e476edff17d
ThomasTartrau <thomas.tartrau@ecoles-epsi.net>
2025-01-09 14:20:19 +0100

    fix(ci): make ci fail on k6 check's fail

--- e2e-tests/main.js
+++ e2e-tests/main.js
@@ -12,6 +12,10 @@ export const options = {
   vus: config.vus,
   duration: config.duration,
   //maxDuration: config.maxDuration,
+  thresholds: {
+    // the rate of successful checks should be at 100%
+    checks: ['rate>=1.0'],
+  },
 };
 
 function isNotNull(value) {
@@ -25,22 +29,19 @@ function scenario_1() {
 
   let application_id = create_application(h, o, s);
   if (!isNotNull(application_id)) {
-    console.error('Failed to create application');
-    return;
+    throw new Error('Failed to create application');
   }
 
   let event_type_1 = create_event_type(h, s, application_id);
   if (!isNotNull(event_type_1)) {
-    console.error('Failed to create event type 1');
     if (config.deleteOnFail) delete_application(h, application_id, s);
-    return;
+    throw new Error('Failed to create event type 1');
   }
 
   let event_type_2 = create_event_type(h, s, application_id);
   if (!isNotNull(event_type_2)) {
-    console.error('Failed to create event type 2');
     if (config.deleteOnFail) delete_application(h, application_id, s);
-    return;
+    throw new Error('Failed to create event type 2');
   }
 
   let subscription_1 = create_subscription(
@@ -53,9 +54,8 @@ function scenario_1() {
     'yes'
   );
   if (!isNotNull(subscription_1)) {
-    console.error('Failed to create subscription');
     if (config.deleteOnFail) delete_application(h, application_id, s);
-    return;
+    throw new Error('Failed to create subscription');
   }
   let subscription_1_id = subscription_1.subscription_id;
   let label_1_key = subscription_1.label_key;
@@ -71,9 +71,8 @@ function scenario_1() {
     'yes'
   );
   if (!isNotNull(subscription_2)) {
-    console.error('Failed to create subscription');
     if (config.deleteOnFail) delete_application(h, application_id, s);
-    return;
+    throw new Error('Failed to create subscription');
   }
   let subscription_2_id = subscription_2.subscription_id;
   let label_2_key = subscription_2.label_key;
@@ -81,67 +80,58 @@ function scenario_1() {
 
   let event_1 = send_event(s, h, application_id, event_type_1, { [label_1_key]: label_1_value });
   if (!isNotNull(event_1)) {
-    console.error('Failed to create event 1');
     if (config.deleteOnFail) delete_application(h, application_id, s);
-    return;
+    throw new Error('Failed to create event 1');
   }
 
   let event_2 = send_event(s, h, application_id, event_type_2, { [label_2_key]: label_2_value });
   if (!isNotNull(event_2)) {
-    console.error('Failed to create event 2');
     if (config.deleteOnFail) delete_application(h, application_id, s);
-    return;
+    throw new Error('Failed to create event 2');
   }
 
   // Event 3 shoudn't be delivered
   let event_3 = send_event(s, h, application_id, event_type_1, { test: 'test' });
   if (!isNotNull(event_3)) {
-    console.error('Failed to create event 3');
     if (config.deleteOnFail) delete_application(h, application_id, s);
-    return;
+    throw new Error('Failed to create event 3');
   }
 
   let request_attempts_1 = list_request_attempt(h, s, application_id, event_1);
   if (!isNotNull(request_attempts_1)) {
-    console.error('Failed to list request attempts 1');
     if (config.deleteOnFail) delete_application(h, application_id, s);
-    return;
+    throw new Error('Failed to list request attempts 1');
   }
   if (request_attempts_1.length !== 2) {
-    console.error(
+    if (config.deleteOnFail) delete_application(h, application_id, s);
+    throw new Error(
       'Expected to find 2 request attempts for event 1 | Found: ' + request_attempts_1.length
     );
-    if (config.deleteOnFail) delete_application(h, application_id, s);
-    return;
   }
 
   let request_attempts_2 = list_request_attempt(h, s, application_id, event_2);
   if (!isNotNull(request_attempts_2)) {
-    console.error('Failed to list request attempts 2');
     if (config.deleteOnFail) delete_application(h, application_id, s);
-    return;
+    throw new Error('Failed to list request attempts 2');
   }
   if (request_attempts_2.length !== 1) {
-    console.error(
+    if (config.deleteOnFail) delete_application(h, application_id, s);
+    throw new Error(
       'Expected to find 1 request attempts for event 2 | Found: ' + request_attempts_2.length
     );
-    if (config.deleteOnFail) delete_application(h, application_id, s);
-    return;
   }
 
   // Event 3 soulnd't be received by any subscription so the length should be 0
   let request_attempts_3 = list_request_attempt(h, s, application_id, event_3);
   if (!isNotNull(request_attempts_3)) {
-    console.error('Failed to list request attempts 3');
     if (config.deleteOnFail) delete_application(h, application_id, s);
-    return;
+    throw new Error('Failed to list request attempts 3');
   }
   if (request_attempts_3.length !== 0) {
-    console.error(
+    if (config.deleteOnFail) delete_application(h, application_id, s);
+    throw new Error(
       'Expected to find 0 request attempts for event 3 | Found: ' + request_attempts_3.length
     );
-    if (config.deleteOnFail) delete_application(h, application_id, s);
-    return;
   }
 }
 
