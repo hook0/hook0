f9ce76073407b5b336c8d6eb7d47fc86319b97d9
ThomasTartrau <thomas.tartrau@ecoles-epsi.net>
2025-01-09 14:41:42 +0100

    fix(ci): add setup part & an env config option

--- e2e-tests/run-main-tests.sh
+++ e2e-tests/run-main-tests.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
+if [ "$ENABLE_SETUP" = "true" ]; then
+  node setup.js
+fi
 k6 run -e "API_ORIGIN=$API_ORIGIN" -e "TARGET_URL=$TARGET_URL" -e "SERVICE_TOKEN=$MASTER_API_KEY" -e "ORGANIZATION_ID=$ORGANIZATION_ID" -e "VUS=$VUS" -e "ITERATIONS=$ITERATIONS" -e "DURATION=$DURATION" main.js
