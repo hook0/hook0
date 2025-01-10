64ec2cde0ab8c972991b20238069d44fecc0de64
ThomasTartrau <thomas.tartrau@ecoles-epsi.net>
2025-01-10 10:01:26 +0100

    refactor: apply suggestions from David's review

--- e2e-tests/run-main-tests.sh
+++ e2e-tests/run-main-tests.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 
-if [ "$ENABLE_SETUP" = "true" ]; then
+set -euo pipefail
+
+if [ "${ENABLE_SETUP:-}" = "true" ]; then
   node setup.js
 fi
-k6 run -e "API_ORIGIN=$API_ORIGIN" -e "TARGET_URL=$TARGET_URL" -e "SERVICE_TOKEN=$MASTER_API_KEY" -e "ORGANIZATION_ID=$ORGANIZATION_ID" -e "VUS=$VUS" -e "ITERATIONS=$ITERATIONS" -e "DURATION=$DURATION" main.js
+
+k6 run \
+  -e "API_ORIGIN=$API_ORIGIN" \
+  -e "TARGET_URL=$TARGET_URL" \
+  -e "SERVICE_TOKEN=$MASTER_API_KEY" \
+  -e "ORGANIZATION_ID=$ORGANIZATION_ID" \
+  -e "VUS=$VUS" \
+  -e "ITERATIONS=$ITERATIONS" \
+  -e "DURATION=$DURATION" \
+  main.js
