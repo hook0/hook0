e7c1a004e66a55285884381a658859b9578c7d59
ThomasTartrau <thomas.tartrau@ecoles-epsi.net>
2025-01-13 15:38:44 +0100

    refactor(ci): david's comment

--- e2e-tests/main.js
+++ e2e-tests/main.js
@@ -27,144 +27,100 @@ function scenario_1() {
   const s = config.serviceToken;
   const o = config.organizationId;
 
-  let application_id = create_application(h, o, s);
-  if (!isNotNull(application_id)) {
-    throw new Error('Failed to create application');
-  }
+  let application_id = null;
 
-  let event_type_1 = create_event_type(h, s, application_id);
-  if (!isNotNull(event_type_1)) {
-    if (!config.keepTestApplication) {
-      delete_application(h, application_id, s);
+  try {
+    application_id = create_application(h, o, s);
+    if (!isNotNull(application_id)) {
+      throw new Error('Failed to create application');
     }
-    throw new Error('Failed to create event type 1');
-  }
 
-  let event_type_2 = create_event_type(h, s, application_id);
-  if (!isNotNull(event_type_2)) {
-    if (!config.keepTestApplication) {
-      delete_application(h, application_id, s);
+    let event_type_1 = create_event_type(h, s, application_id);
+    if (!isNotNull(event_type_1)) {
+      throw new Error('Failed to create event type 1');
     }
-    throw new Error('Failed to create event type 2');
-  }
 
-  let subscription_1 = create_subscription(
-    h,
-    s,
-    application_id,
-    [event_type_1, event_type_2],
-    config.targetUrl,
-    'all',
-    'yes'
-  );
-  if (!isNotNull(subscription_1)) {
-    if (!config.keepTestApplication) {
-      delete_application(h, application_id, s);
+    let event_type_2 = create_event_type(h, s, application_id);
+    if (!isNotNull(event_type_2)) {
+      throw new Error('Failed to create event type 2');
     }
-    throw new Error('Failed to create subscription');
-  }
-  let subscription_1_id = subscription_1.subscription_id;
-  let label_1_key = subscription_1.label_key;
-  let label_1_value = subscription_1.label_value;
 
-  let subscription_2 = create_subscription(
-    h,
-    s,
-    application_id,
-    [event_type_1],
-    config.targetUrl,
-    'all',
-    'yes'
-  );
-  if (!isNotNull(subscription_2)) {
-    if (!config.keepTestApplication) {
-      delete_application(h, application_id, s);
-    }
-    throw new Error('Failed to create subscription');
-  }
-  let subscription_2_id = subscription_2.subscription_id;
-  let label_2_key = subscription_2.label_key;
-  let label_2_value = subscription_2.label_value;
-
-  let event_1 = send_event(s, h, application_id, event_type_1, { [label_1_key]: label_1_value });
-  if (!isNotNull(event_1)) {
-    if (!config.keepTestApplication) {
-      delete_application(h, application_id, s);
-    }
-    throw new Error('Failed to create event 1');
-  }
-
-  let event_2 = send_event(s, h, application_id, event_type_2, { [label_2_key]: label_2_value });
-  if (!isNotNull(event_2)) {
-    if (!config.keepTestApplication) {
-      delete_application(h, application_id, s);
-    }
-    throw new Error('Failed to create event 2');
-  }
-
-  // Event 3 shouldn't be delivered
-  let event_3 = send_event(s, h, application_id, event_type_1, { test: 'test' });
-  if (!isNotNull(event_3)) {
-    if (!config.keepTestApplication) {
-      delete_application(h, application_id, s);
-    }
-    throw new Error('Failed to create event 3');
-  }
-
-  let request_attempts_1 = list_request_attempt(h, s, application_id, event_1);
-  if (!isNotNull(request_attempts_1)) {
-    if (!config.keepTestApplication) {
-      delete_application(h, application_id, s);
-    }
-    throw new Error('Failed to list request attempts 1');
-  }
-  if (request_attempts_1.length !== 2) {
-    if (!config.keepTestApplication) {
-      delete_application(h, application_id, s);
-    }
-    throw new Error(
-      'Expected to find 2 request attempts for event 1 | Found: ' + request_attempts_1.length
+    let subscription_1 = create_subscription(
+      h,
+      s,
+      application_id,
+      [event_type_1, event_type_2],
+      config.targetUrl,
+      'all',
+      'yes'
     );
-  }
+    if (!isNotNull(subscription_1)) {
+      throw new Error('Failed to create subscription 1');
+    }
 
-  let request_attempts_2 = list_request_attempt(h, s, application_id, event_2);
-  if (!isNotNull(request_attempts_2)) {
-    if (!config.keepTestApplication) {
-      delete_application(h, application_id, s);
-    }
-    throw new Error('Failed to list request attempts 2');
-  }
-  if (request_attempts_2.length !== 1) {
-    if (!config.keepTestApplication) {
-      delete_application(h, application_id, s);
-    }
-    throw new Error(
-      'Expected to find 1 request attempts for event 2 | Found: ' + request_attempts_2.length
+    let subscription_2 = create_subscription(
+      h,
+      s,
+      application_id,
+      [event_type_1],
+      config.targetUrl,
+      'all',
+      'yes'
     );
-  }
+    if (!isNotNull(subscription_2)) {
+      throw new Error('Failed to create subscription 2');
+    }
 
-  // Event 3 shouldn't be received by any subscription so the length should be 0
-  let request_attempts_3 = list_request_attempt(h, s, application_id, event_3);
-  if (!isNotNull(request_attempts_3)) {
-    if (!config.keepTestApplication) {
+    let event_1 = send_event(s, h, application_id, event_type_1, {
+      [subscription_1.label_key]: subscription_1.label_value,
+    });
+    if (!isNotNull(event_1)) {
+      throw new Error('Failed to create event 1');
+    }
+
+    let event_2 = send_event(s, h, application_id, event_type_2, {
+      [subscription_2.label_key]: subscription_2.label_value,
+    });
+    if (!isNotNull(event_2)) {
+      throw new Error('Failed to create event 2');
+    }
+
+    let event_3 = send_event(s, h, application_id, event_type_1, { test: 'test' });
+    if (!isNotNull(event_3)) {
+      throw new Error('Failed to create event 3');
+    }
+
+    let request_attempts_1 = list_request_attempt(h, s, application_id, event_1);
+    if (!isNotNull(request_attempts_1) || request_attempts_1.length !== 2) {
+      throw new Error(
+        'Expected to find 2 request attempts for event 1 | Found: ' + request_attempts_1.length
+      );
+    }
+
+    let request_attempts_2 = list_request_attempt(h, s, application_id, event_2);
+    if (!isNotNull(request_attempts_2) || request_attempts_2.length !== 1) {
+      throw new Error(
+        'Expected to find 1 request attempts for event 2 | Found: ' + request_attempts_2.length
+      );
+    }
+
+    let request_attempts_3 = list_request_attempt(h, s, application_id, event_3);
+    if (!isNotNull(request_attempts_3) || request_attempts_3.length !== 0) {
+      throw new Error(
+        'Expected to find 0 request attempts for event 3 | Found: ' + request_attempts_3.length
+      );
+    }
+  } catch (error) {
+    console.error(error.message);
+    throw error;
+  } finally {
+    if (application_id && !config.keepTestApplication) {
       delete_application(h, application_id, s);
     }
-    throw new Error('Failed to list request attempts 3');
-  }
-  if (request_attempts_3.length !== 0) {
-    if (!config.keepTestApplication) {
-      delete_application(h, application_id, s);
-    }
-    throw new Error(
-      'Expected to find 0 request attempts for event 3 | Found: ' + request_attempts_3.length
-    );
-  }
-
-  if (!config.keepTestApplication) {
-    delete_application(h, application_id, s);
   }
 }
 
 export default function () {
   scenario_1();
 }
+
