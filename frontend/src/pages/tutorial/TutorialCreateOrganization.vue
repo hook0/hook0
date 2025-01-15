49c5b94ddfb46a8326733a20d35a218eb7d5f03d
ThomasTartrau <thomas.tartrau@ecoles-epsi.net>
2025-01-15 11:54:09 +0100

    refactor(frontend)

--- frontend/src/pages/tutorial/TutorialCreateOrganization.vue
+++ frontend/src/pages/tutorial/TutorialCreateOrganization.vue
@@ -5,7 +5,7 @@ import Hook0CardHeader from '@/components/Hook0CardHeader.vue';
 import Hook0CardContent from '@/components/Hook0CardContent.vue';
 import OrganizationsEdit from '@/pages/organizations/OrganizationsEdit.vue';
 import Hook0Card from '@/components/Hook0Card.vue';
-import { onMounted, onUpdated, ref } from 'vue';
+import { onMounted, ref } from 'vue';
 import Hook0CardFooter from '@/components/Hook0CardFooter.vue';
 import Hook0Button from '@/components/Hook0Button.vue';
 import { routes } from '@/routes.ts';
@@ -17,7 +17,6 @@ import Hook0Loader from '@/components/Hook0Loader.vue';
 import Hook0Error from '@/components/Hook0Error.vue';
 import Hook0Select from '@/components/Hook0Select.vue';
 import Hook0ProgressBar from '@/components/Hook0ProgressBar.vue';
-import Hook0Input from '@/components/Hook0Input.vue';
 import party from 'party-js';
 import { progressItems } from '@/pages/tutorial/TutorialService';
 
@@ -31,7 +30,6 @@ enum Sections {
 const organizationId = ref<UUID | null>(null);
 const organizations_list = ref<Promise<Array<{ label: string; value: UUID }>>>(Promise.resolve([]));
 const selected_organization_id = ref<UUID | null>(null);
-
 const currentSection = ref<Sections | null>(null);
 
 const goSecondStep = (organization_id: UUID) => {
@@ -100,10 +98,6 @@ function displayError(err: Problem) {
 onMounted(() => {
   _onLoad();
 });
-
-onUpdated(() => {
-  _onLoad();
-});
 </script>
 
 <template>
@@ -112,106 +106,98 @@ onUpdated(() => {
       <template #header>
         <div class="flex items-center justify-between">Step 1: Create your first organization</div>
       </template>
-      <template #subtitle
-        >Organizations are used to invite your team members and give them access to applications,
+      <template #subtitle>
+        Organizations are used to invite your team members and give them access to applications,
         where everything webhook-related happens. If you purchase a plan, it will be applied to an
         organization and not to a user nor an application.
       </template>
     </Hook0CardHeader>
     <Hook0CardContent>
-      <Hook0CardContentLines>
-        <Hook0CardContentLine type="full-width">
-          <template #content>
-            <Hook0ProgressBar actual="1" :items="progressItems" class="mb-14" />
-            <Hook0Card v-if="!organizationId" class="mb-4">
-              <Hook0CardHeader>
-                <template #header>First, let's choose an organization!</template>
-              </Hook0CardHeader>
-              <Hook0CardContent class="p-4 border space-y-4">
-                <div class="grid grid-cols-1 sm:grid-cols-2">
-                  <div
-                    class="flex items-center p-4 w-full border-b sm:border-b-0 sm:border-r cursor-pointer"
-                    @click="
-                      currentSection =
-                        currentSection === Sections.CreateOrganization
-                          ? null
-                          : Sections.CreateOrganization
-                    "
-                  >
-                    <Hook0Input
-                      id="section_create"
-                      type="checkbox"
-                      :value="currentSection === Sections.CreateOrganization"
-                      class="h-4 w-4 border-gray-300 rounded focus:ring-indigo-500"
-                    />
-                    <label for="section_create" class="ml-2 text-sm font-medium text-gray-700">
-                      Create a new organization
-                    </label>
-                  </div>
-                  <div
-                    class="flex items-center p-4 w-full border-t sm:border-t-0 sm:border-l cursor-pointer"
-                    @click="
-                      currentSection =
-                        currentSection === Sections.SelectExistingOrganization
-                          ? null
-                          : Sections.SelectExistingOrganization
-                    "
-                  >
-                    <Hook0Input
-                      id="section_select"
-                      type="checkbox"
-                      :value="currentSection === Sections.SelectExistingOrganization"
-                      class="h-4 w-4 border-gray-300 rounded focus:ring-indigo-500"
-                    />
-                    <label for="section_select" class="ml-2 text-sm font-medium text-gray-700">
-                      Select an existing organization already linked to your account
-                    </label>
-                  </div>
-                </div>
-              </Hook0CardContent>
-            </Hook0Card>
-            <OrganizationsEdit
-              v-if="!organizationId && currentSection === Sections.CreateOrganization"
-              :tutorial-mode="true"
-              class="mb-4"
-              @tutorial-organization-created="goSecondStep($event)"
-            />
-          </template>
-        </Hook0CardContentLine>
-        <Promised
-          v-if="currentSection === Sections.SelectExistingOrganization"
-          :promise="organizations_list"
-        >
-          <template #pending>
-            <Hook0Loader></Hook0Loader>
-          </template>
-          <template #default="organizations">
+      <Hook0CardContentLine type="full-width">
+        <template #content>
+          <Hook0ProgressBar actual="1" :items="progressItems" class="mb-14" />
+          <Hook0Card v-if="!organizationId" class="mb-4">
+            <Hook0CardHeader>
+              <template #header>First, let's choose an organization!</template>
+            </Hook0CardHeader>
+            <Hook0CardContent class="p-4 border space-y-4">
+              <div class="grid grid-cols-1 sm:grid-cols-2">
+                <label
+                  class="flex items-center p-4 w-full border-b sm:border-b-0 sm:border-r cursor-pointer"
+                >
+                  <input
+                    v-model="currentSection"
+                    type="radio"
+                    name="organization_selection"
+                    :value="Sections.CreateOrganization"
+                    class="h-4 w-4 border-gray-300 rounded focus:ring-indigo-500"
+                  />
+                  <span class="ml-2 text-sm font-medium text-gray-700">
+                    Create a new organization
+                  </span>
+                </label>
+                <label
+                  class="flex items-center p-4 w-full border-t sm:border-t-0 sm:border-l cursor-pointer"
+                >
+                  <input
+                    v-model="currentSection"
+                    type="radio"
+                    name="organization_selection"
+                    :value="Sections.SelectExistingOrganization"
+                    class="h-4 w-4 border-gray-300 rounded focus:ring-indigo-500"
+                  />
+                  <span class="ml-2 text-sm font-medium text-gray-700">
+                    Select an existing organization already linked to your account
+                  </span>
+                </label>
+              </div>
+            </Hook0CardContent>
+          </Hook0Card>
+          <OrganizationsEdit
+            v-if="!organizationId && currentSection === Sections.CreateOrganization"
+            :tutorial-mode="true"
+            class="mt-12"
+            @tutorial-organization-created="goSecondStep($event)"
+          />
+        </template>
+      </Hook0CardContentLine>
+      <Promised
+        v-if="currentSection === Sections.SelectExistingOrganization"
+        :promise="organizations_list"
+      >
+        <template #pending>
+          <Hook0Loader></Hook0Loader>
+        </template>
+        <template #default="organizations">
+          <div class="px-6">
             <Hook0Card>
               <Hook0CardContent>
-                <Hook0CardContentLine>
-                  <template #label>
-                    You can also select an existing organization to continue the tutorial.
-                  </template>
-                  <template #content>
-                    <Hook0Select
-                      v-model="selected_organization_id"
-                      :options="organizations"
-                    ></Hook0Select>
-                  </template>
-                </Hook0CardContentLine>
+                <Hook0CardContentLines>
+                  <Hook0CardContentLine type="full-width">
+                    <template #label>
+                      You can also select an existing organization to continue the tutorial.
+                    </template>
+                    <template #content>
+                      <Hook0Select
+                        v-model="selected_organization_id"
+                        :options="organizations"
+                      ></Hook0Select>
+                    </template>
+                  </Hook0CardContentLine>
+                </Hook0CardContentLines>
               </Hook0CardContent>
             </Hook0Card>
-          </template>
-          <template #rejected="error">
-            <Hook0Error :error="error"></Hook0Error>
-          </template>
-        </Promised>
-      </Hook0CardContentLines>
+          </div>
+        </template>
+        <template #rejected="error">
+          <Hook0Error :error="error"></Hook0Error>
+        </template>
+      </Promised>
     </Hook0CardContent>
     <Hook0CardFooter v-if="organizationId || selected_organization_id">
-      <Hook0Button class="primary" type="button" @click="goSecondStep"
-        >🚀 Continue Step 2: Create Your Application</Hook0Button
-      >
+      <Hook0Button class="primary" type="button" @click="goSecondStep">
+        🚀 Continue Step 2: Create Your Application
+      </Hook0Button>
     </Hook0CardFooter>
   </Hook0Card>
 </template>
