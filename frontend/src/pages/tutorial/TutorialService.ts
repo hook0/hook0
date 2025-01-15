f4461f1a0766e0f8dfc8d0d210c1e0dcfc921c10
ThomasTartrau <thomas.tartrau@ecoles-epsi.net>
2025-01-15 10:56:07 +0100

    refactor(frontend): change icons for use the actual's one

--- frontend/src/pages/tutorial/TutorialService.ts
+++ frontend/src/pages/tutorial/TutorialService.ts
@@ -1,9 +1,9 @@
 import { ref } from 'vue';
 
 export const progressItems = ref([
-  { icon: 'building', description: 'Organization' },
-  { icon: 'terminal', description: 'Application' },
-  { icon: 'list-check', description: 'Event Type' },
-  { icon: 'location-dot', description: 'Subscription' },
-  { icon: 'envelope', description: 'Event' },
+  { icon: 'sitemap', description: 'Organization' },
+  { icon: 'rocket', description: 'Application' },
+  { icon: 'folder-tree', description: 'Event Type' },
+  { icon: 'link', description: 'Subscription' },
+  { icon: 'file-lines', description: 'Event' },
 ]);
