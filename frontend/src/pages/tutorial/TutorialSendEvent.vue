b6d7845803875e0417b2177a5c780505d4dca5f9
ThomasTartrau <thomas.tartrau@ecoles-epsi.net>
2025-01-16 16:42:45 +0100

    feat/fix(frontend)

--- frontend/src/pages/tutorial/TutorialSendEvent.vue
+++ frontend/src/pages/tutorial/TutorialSendEvent.vue
@@ -114,8 +114,20 @@ onMounted(() => {
         </Hook0CardContentLine>
       </Hook0CardContentLines>
     </Hook0CardContent>
-    <Hook0CardFooter v-if="!disabled_button">
+    <Hook0CardFooter>
       <Hook0Button
+        class="secondary"
+        type="button"
+        @click="
+          router.push({
+            name: routes.ApplicationsDashboard,
+            params: { organization_id: organizationId, application_id: applicationId },
+          })
+        "
+        >Skip</Hook0Button
+      >
+      <Hook0Button
+        v-if="!disabled_button"
         class="primary"
         type="button"
         :disabled="disabled_button"
