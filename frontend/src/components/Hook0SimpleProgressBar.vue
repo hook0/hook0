7bae3d44cde2fcd3121303cf7c40e6b4903cea69
ThomasTartrau <thomas.tartrau@ecoles-epsi.net>
2025-01-28 17:04:24 +0100

    feat(frontend): add color changes on 80% and 100% on quotas progress bar

--- frontend/src/components/Hook0SimpleProgressBar.vue
+++ frontend/src/components/Hook0SimpleProgressBar.vue
@@ -12,7 +12,13 @@ const props = defineProps<Props>();
       class="text-md text-center p-1 leading-none rounded-lg font-extrabold"
       :class="[
         props.percentage < 3 ? 'text-black' : 'text-white',
-        props.percentage > 0 ? 'bg-indigo-500 ' : '',
+        props.percentage > 0
+          ? props.percentage >= 80
+            ? props.percentage >= 100
+              ? 'bg-red-500'
+              : 'bg-orange-500'
+            : 'bg-indigo-500'
+          : '',
       ]"
       :style="'width: ' + (props.percentage >= 100 ? 100 : props.percentage) + '%'"
     >
