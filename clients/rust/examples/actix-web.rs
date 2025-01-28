6da97d8c171b4e7ca62fcf86d1bfc01516e41dc4
ThomasTartrau <thomas.tartrau@ecoles-epsi.net>
2025-01-28 11:08:21 +0100

    refactor(clients)

--- clients/rust/examples/actix-web.rs
+++ clients/rust/examples/actix-web.rs
@@ -1,39 +1,20 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder};
-use chrono::Duration;
-use hook0_client::{Hook0Client, Hook0ClientError};
-use std::sync::Arc;
+use std::time::Duration;
 
-const API_URL: &str = "http://localhost:8080";
-const APPLICATION_ID: &str = "7cab1450-5820-471e-9396-4ee87384d535";
+use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+use hook0_client::Hook0ClientError;
+
 const SUBSCRIPTION_SECRET: &str = "ebc17f0b-566e-4d02-be72-df8ec3a6d16c";
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let client = Hook0Client::new(
-        API_URL.parse().unwrap(),
-        uuid::Uuid::parse_str(APPLICATION_ID).unwrap(),
-        SUBSCRIPTION_SECRET,
-    )
-    .unwrap();
-
-    let shared_client = web::Data::new(Arc::new(client));
-
-    HttpServer::new(move || {
-        App::new()
-            .app_data(shared_client.clone())
-            .route("/webhook", web::post().to(handle_webhook))
-    })
-    .bind("127.0.0.1:8081")?
-    .workers(1)
-    .run()
-    .await
+    HttpServer::new(move || App::new().route("/webhook", web::post().to(handle_webhook)))
+        .bind("127.0.0.1:8081")?
+        .workers(1)
+        .run()
+        .await
 }
 
-async fn handle_webhook(
-    req: HttpRequest,
-    body: web::Bytes,
-    client: web::Data<Arc<Hook0Client>>,
-) -> impl Responder {
+async fn handle_webhook(req: HttpRequest, body: web::Bytes) -> impl Responder {
     let content_type = req.headers().get("Content-Type");
     let event_id = req.headers().get("X-Event-Id");
     let event_type = req.headers().get("X-Event-Type");
@@ -48,15 +29,33 @@ async fn handle_webhook(
 
     if let Some(signature) = signature {
         let signature: &str = signature.to_str().unwrap();
-        let tolerance = Duration::seconds(300);
+        let tolerance = Duration::from_secs(300);
 
-        match client.verifying_webhook_signature(signature, &body, SUBSCRIPTION_SECRET, tolerance) {
+        match hook0_client::verify_webhook_signature(
+            signature,
+            &body,
+            SUBSCRIPTION_SECRET,
+            tolerance,
+        ) {
             Ok(_) => println!("Signature verification successful!"),
             Err(Hook0ClientError::InvalidSignature) => {
                 println!("Signature verification failed: Invalid signature.")
             }
-            Err(Hook0ClientError::ExpiredWebhook) => {
-                println!("Signature verification failed: Webhook expired.")
+            Err(Hook0ClientError::ExpiredWebhook {
+                signed_at,
+                tolerance,
+                current_time,
+            }) => {
+                println!("Signature verification failed: The webhook has expired because it was sent too long ago (signed_at={signed_at}, tolerance={tolerance}, current_time={current_time})")
+            }
+            Err(Hook0ClientError::SignatureParsing(signature)) => {
+                println!("Signature verification failed: Could not parse signature: {signature}")
+            }
+            Err(Hook0ClientError::TimestampParsingInSignature(timestamp)) => {
+                println!("Signature verification failed: Could not parse timestamp in signature: {timestamp}")
+            }
+            Err(Hook0ClientError::InvalidTolerance(err)) => {
+                println!("Signature verification failed: Invalid tolerance: {err}")
             }
             Err(err) => println!("Signature verification failed: {err}"),
         }
