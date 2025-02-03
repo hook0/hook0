db3494ca8abeffbad7893dcd436b22c1291b603a
ThomasTartrau <thomas.tartrau@ecoles-epsi.net>
2025-02-03 12:47:59 +0100

    fix(e2e): change the json format check (been changed in the api)

--- e2e-tests/src/unauthentified/quotas.js
+++ e2e-tests/src/unauthentified/quotas.js
@@ -24,25 +24,26 @@ export default function (baseUrl) {
     !check(json, {
       'quotas.enabled': () => typeof json.enabled === 'boolean',
       'quotas.global_members_per_organization_limit': () =>
-        typeof json.global_members_per_organization_limit === 'number' &&
-        json.global_members_per_organization_limit > 0,
+        typeof json.limits.global_members_per_organization_limit === 'number' &&
+        json.limits.global_members_per_organization_limit >= 1,
       'quotas.global_applications_per_organization_limit': () =>
-        typeof json.global_applications_per_organization_limit === 'number' &&
-        json.global_applications_per_organization_limit > 0,
+        typeof json.limits.global_applications_per_organization_limit === 'number' &&
+        json.limits.global_applications_per_organization_limit >= 1,
       'quotas.global_events_per_day_limit': () =>
-        typeof json.global_events_per_day_limit === 'number' &&
-        json.global_events_per_day_limit > 0,
+        typeof json.limits.global_events_per_day_limit === 'number' &&
+        json.limits.global_events_per_day_limit >= 1,
       'quotas.global_days_of_events_retention_limit': () =>
-        typeof json.global_days_of_events_retention_limit === 'number' &&
-        json.global_days_of_events_retention_limit > 0,
+        typeof json.limits.global_days_of_events_retention_limit === 'number' &&
+        json.limits.global_days_of_events_retention_limit >= 1,
       'quotas.global_subscriptions_per_application_limit': () =>
-        typeof json.global_subscriptions_per_application_limit === 'number' &&
-        json.global_subscriptions_per_application_limit > 0,
+        typeof json.limits.global_subscriptions_per_application_limit === 'number' &&
+        json.limits.global_subscriptions_per_application_limit >= 1,
       'quotas.global_event_types_per_application_limit': () =>
-        typeof json.global_event_types_per_application_limit === 'number' &&
-        json.global_event_types_per_application_limit > 0,
+        typeof json.limits.global_event_types_per_application_limit === 'number' &&
+        json.limits.global_event_types_per_application_limit >= 1,
     })
   ) {
+    console.log(`Received quotas: ${JSON.stringify(json.limits, null, 2)}`);
     throw new Error(`Unexpected response: ${JSON.stringify(json)}`);
   }
 
