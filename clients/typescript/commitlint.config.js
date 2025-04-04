60760d4d7e2147cbd584cb0caeafc20ee9ff09df
Thomas Tartrau <145434513+ThomasTartrau@users.noreply.github.com>
2025-04-04 12:54:53 +0200

    chore: change the sementic release rules

---
+++ clients/typescript/commitlint.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    extends: ["@commitlint/config-conventional"],
+    rules: {
+      "body-max-line-length": [2, "always", 200],
+      "footer-max-line-length": [2, "always", 200],
+    },
+  };
+  
