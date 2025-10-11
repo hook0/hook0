16cd18a8e752f87a190a41565f91d034925ea488
David Sferruzza <david.sferruzza@gmail.com>
2025-10-11 16:42:55 +0200

    fix(api): index name

--- api/migrations/20251011130707_add_index_to_improve_removing_dangling_responses.up.sql
+++ api/migrations/20251011130707_add_index_to_improve_removing_dangling_responses.up.sql
@@ -1,1 +1,1 @@
-create index if not exists request_attempt_no_response on webhook.request_attempt (response__id) where response__id is null;
+create index if not exists request_attempt_no_response_idx on webhook.request_attempt (response__id) where response__id is null;
