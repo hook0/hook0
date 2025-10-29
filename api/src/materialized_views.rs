c1131488baa6789e04e1f7eb6dbbaf5a4e675769
David Sferruzza <david.sferruzza@gmail.com>
2025-10-29 19:50:15 +0100

    fix(api): timeout for refreshing materialized views

--- api/src/materialized_views.rs
+++ api/src/materialized_views.rs
@@ -1,4 +1,4 @@
-use actix_web::rt::time::{sleep, timeout};
+use actix_web::rt::time::sleep;
 use log::{debug, error, trace};
 use sqlx::{PgPool, query};
 use std::time::{Duration, Instant};
@@ -11,16 +11,12 @@ pub async fn periodically_refresh_materialized_views(
     db: &PgPool,
     period: Duration,
 ) {
-    let timeout_duration = period / 2;
+    let timeout = period / 2;
     sleep(STARTUP_GRACE_PERIOD).await;
 
     while let Ok(permit) = housekeeping_semaphore.acquire().await {
-        match timeout(timeout_duration, refresh_materialized_views(db)).await {
-            Err(_) => {
-                error!("Timed out after {timeout_duration:?} while refreshing materialized views")
-            }
-            Ok(Err(e)) => error!("Could not refresh materialized views: {e}"),
-            Ok(_) => (),
+        if let Err(e) = refresh_materialized_views(db, timeout).await {
+            error!("Could not refresh materialized views: {e}")
         }
 
         drop(permit);
@@ -29,12 +25,26 @@ pub async fn periodically_refresh_materialized_views(
     }
 }
 
-async fn refresh_materialized_views(db: &PgPool) -> Result<(), sqlx::Error> {
+async fn refresh_materialized_views(db: &PgPool, timeout: Duration) -> Result<(), sqlx::Error> {
     trace!("Refreshing materialized view event.events_per_day...");
     let start = Instant::now();
+
+    let mut tx = db.begin().await?;
+    query(&format!(
+        "SET LOCAL statement_timeout = '{}s'",
+        timeout.as_secs()
+    ))
+    .execute(&mut *tx)
+    .await?;
     query!("REFRESH MATERIALIZED VIEW CONCURRENTLY event.events_per_day")
+        .execute(&mut *tx)
+        .await?;
+    tx.commit().await?;
+
+    query!("VACUUM ANALYZE event.events_per_day")
         .execute(db)
         .await?;
+
     debug!(
         "Materialized view event.events_per_day was refreshed in {:?}",
         start.elapsed()
