3d36dbde15d2b31820e2246a23650eb706d3e625
David Sferruzza <david.sferruzza@gmail.com>
2025-12-01 22:45:59 +0100

    feat(api): report span when refreshing materialized views

--- api/src/materialized_views.rs
+++ api/src/materialized_views.rs
@@ -1,5 +1,8 @@
 use actix_web::rt::time::sleep;
+use clap::crate_name;
 use log::{debug, error, trace};
+use opentelemetry::trace::Tracer;
+use opentelemetry::{global, trace::TraceContextExt};
 use sqlx::{PgPool, query};
 use std::time::{Duration, Instant};
 use tokio::sync::Semaphore;
@@ -26,28 +29,34 @@ pub async fn periodically_refresh_materialized_views(
 }
 
 async fn refresh_materialized_views(db: &PgPool, timeout: Duration) -> Result<(), sqlx::Error> {
-    trace!("Refreshing materialized view event.events_per_day...");
-    let start = Instant::now();
+    global::tracer(crate_name!())
+        .in_span("refresh_materialized_views", |cx| async move {
+            trace!("Refreshing materialized view event.events_per_day...");
+            let start = Instant::now();
 
-    let mut tx = db.begin().await?;
-    query(&format!(
-        "SET LOCAL statement_timeout = '{}s'",
-        timeout.as_secs()
-    ))
-    .execute(&mut *tx)
-    .await?;
-    query!("REFRESH MATERIALIZED VIEW CONCURRENTLY event.events_per_day")
-        .execute(&mut *tx)
-        .await?;
-    tx.commit().await?;
+            let mut tx = db.begin().await?;
+            query(&format!(
+                "SET LOCAL statement_timeout = '{}s'",
+                timeout.as_secs()
+            ))
+            .execute(&mut *tx)
+            .await?;
+            query!("REFRESH MATERIALIZED VIEW CONCURRENTLY event.events_per_day")
+                .execute(&mut *tx)
+                .await?;
+            tx.commit().await?;
+            cx.span().add_event("events_per_day.refreshed", Vec::new());
 
-    query!("VACUUM ANALYZE event.events_per_day")
-        .execute(db)
-        .await?;
+            query!("VACUUM ANALYZE event.events_per_day")
+                .execute(db)
+                .await?;
+            cx.span().add_event("events_per_day.vacuumed", Vec::new());
 
-    debug!(
-        "Materialized view event.events_per_day was refreshed in {:?}",
-        start.elapsed()
-    );
-    Ok(())
+            debug!(
+                "Materialized view event.events_per_day was refreshed in {:?}",
+                start.elapsed()
+            );
+            Ok(())
+        })
+        .await
 }
