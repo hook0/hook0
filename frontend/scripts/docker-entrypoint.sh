448d0572d5af03500107aff9129df8f562d351af
David Sferruzza <david.sferruzza@gmail.com>
2026-01-31 18:23:46 +0100

    feat(frontend): allow to set API_ENDPOINT and API_TIMEOUT at runtime when using Docker image

--- frontend/scripts/docker-entrypoint.sh
+++ frontend/scripts/docker-entrypoint.sh
@@ -4,5 +4,8 @@ set -eu
 # Set the right API endpoint URL (app was build with a placeholder so that the value can be set without rebuilding)
 find /usr/share/caddy/ -name "*.js" -exec sed -i "s|API_ENDPOINT_PLACEHOLDER|${API_ENDPOINT}|g" {} \;
 
+# Set the right API timeout duration (app was build with a placeholder so that the value can be set without rebuilding)
+find /usr/share/caddy/ -name "*.js" -exec sed -i "s|API_TIMEOUT_PLACEHOLDER|${API_TIMEOUT:-3000}|g" {} \;
+
 # Run Caddy to serve the app
 caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
