814db6ed25799c18fb2f1fe18e9a58247b204a64
Romain Baumier <baumier.romain@gmail.com>
2026-03-21 10:58:34 +0100

    Reapply "chore(frontend): remove redundant Vite built-in types from env.d.ts"
    This reverts commit c86b5196509887a764b75d4b78b548121471865a.

--- frontend/src/env.d.ts
+++ frontend/src/env.d.ts
@@ -1,7 +1,4 @@
 interface ImportMetaEnv {
-  readonly NODE_ENV: 'development' | 'production' | 'test';
-  readonly DEV: boolean;
-  readonly PROD: boolean;
   readonly VITE_API_TIMEOUT: string;
   readonly VITE_API_ENDPOINT: string;
   readonly VITE_CRISP_WEBSITE_ID?: string;
